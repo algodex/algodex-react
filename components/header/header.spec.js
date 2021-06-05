1795a47f6230361ebb68eb0598c29af35e50de30
Dominic Hawton <hello@dominichawton.com>
2021-06-05 20:21:01 +1000

    test(orders):

--- components/header/header.spec.js
+++ components/header/header.spec.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import { render, screen } from '../../test/test-utils'
-import Nav from './index'
+import Header from './index'
 
-describe('Nav', () => {})
+it('Header: default', () => {
+  const component = renderer.create(<Header />)
+  const tree = component.toJSON()
+  expect(tree).toMatchSnapshot()
+})
