0b49e78105d81c876f03addc2aa8976c63c7277b
Doug Richar <drichar@gmail.com>
2021-06-07 23:16:35 -0400

    fix(place order): :white_check_mark: fix failing test

--- components/place-order/place-order.spec.js
+++ components/place-order/place-order.spec.js
@@ -6,7 +6,8 @@ const PLACE_ORDER = 'place-order'
 
 describe('PlaceOrder', () => {
   it('should render', () => {
-    const { getByTestId } = render(<PlaceOrder />)
+    const activeWallet = { id: 'wallet-01', name: 'Main', balance: 812569.2658 }
+    const { getByTestId } = render(<PlaceOrder activeWallet={activeWallet} asset="FAME" />)
 
     expect(getByTestId(PLACE_ORDER)).toBeVisible()
   })
