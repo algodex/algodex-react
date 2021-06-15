88caad0aa16ebccfec89c6094a269c7df02cd4c6
Dominic Hawton <hello@dominichawton.com>
2021-06-15 20:50:23 +1000

    test: :white_check_mark: add transformIgnorePattern to jest config to prevent errors with lightweight-charts package

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,9 @@
+const esModules = ['fancy-canvas', 'lightweight-charts'].join('|')
 module.exports = {
   setupFilesAfterEnv: ['./jest.setup.js'],
   testEnvironment: 'jsdom',
   moduleNameMapper: {
     '^components(.*)$': '<rootDir>/components$1'
-  }
+  },
+  transformIgnorePatterns: [`/node_modules/(?!${esModules})`]
 }
