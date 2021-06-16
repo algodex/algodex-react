f3433848aac3f8130bb7748a86956221c3191ecf
Doug Richar <drichar@gmail.com>
2021-06-16 00:10:43 -0400

    refactor(chart): :recycle: refactor chart component, fix (?) dupe issue

---
+++ components/chart/use-area-chart.js
@@ -0,0 +1,83 @@
+import { useState, useEffect } from 'react'
+import { addListener, removeListener } from 'resize-detector'
+
+const LINE_COLOR = '#1A202C'
+const BACKGROUND_COLOR = '#171923'
+const BORDER_COLOR = '#718096'
+const TEXT_COLOR = '#CBD5E0'
+
+const TOP_COLOR = '#248350'
+const TOP_LINE_COLOR = '#38A169'
+const BOTTOM_COLOR = '#38a16911'
+const LINE_WIDTH = 2
+
+export default function useAreaChart(containerRef, volumeData, priceData) {
+  const [chart, setChart] = useState()
+
+  useEffect(() => {
+    const initializeChart = async () => {
+      const { createChart, CrosshairMode } = await import('lightweight-charts')
+
+      setChart(
+        createChart(containerRef.current, {
+          layout: {
+            backgroundColor: BACKGROUND_COLOR,
+            textColor: TEXT_COLOR,
+            fontFamily: 'Inter'
+          },
+          grid: {
+            vertLines: {
+              color: LINE_COLOR
+            },
+            horzLines: {
+              color: LINE_COLOR
+            }
+          },
+          crosshair: {
+            mode: CrosshairMode.Normal
+          },
+          rightPriceScale: {
+            borderColor: BORDER_COLOR
+          },
+          timeScale: {
+            borderColor: BORDER_COLOR
+          }
+        })
+      )
+    }
+    containerRef?.current && !chart && initializeChart()
+  }, [chart, containerRef])
+
+  useEffect(() => {
+    const chartContainer = containerRef?.current
+
+    if (chart) {
+      const areaSeries = chart.addAreaSeries({
+        topColor: TOP_COLOR,
+        bottomColor: BOTTOM_COLOR,
+        lineColor: TOP_LINE_COLOR,
+        lineWidth: LINE_WIDTH
+      })
+
+      const areaSeriesData = priceData.map(({ time, close }) => ({
+        time,
+        value: close
+      }))
+
+      areaSeries.setData(areaSeriesData)
+      chart.timeScale().fitContent()
+
+      if (chartContainer) {
+        addListener(chartContainer, () =>
+          chart.resize(chartContainer.offsetWidth, chartContainer.offsetHeight)
+        )
+      }
+
+      return () => removeListener(chartContainer)
+    }
+  }, [chart, containerRef, volumeData, priceData])
+
+  return {
+    areaChart: chart
+  }
+}
