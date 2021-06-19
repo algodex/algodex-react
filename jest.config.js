d77d5bb8871269353ee6c97f5749db94f998a108
Dominic Hawton <hello@dominichawton.com>
2021-06-19 13:16:28 +1000

    ci: :green_heart: fix ci

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,9 @@
 module.exports = {
   setupFilesAfterEnv: ['./jest.setup.js'],
+  moduleDirectories: ['node_modules', '.'],
   testEnvironment: 'jsdom',
   moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
     '^components(.*)$': '<rootDir>/components$1'
   },
   transformIgnorePatterns: ['node_modules/(?!(lightweight-charts|fancy-canvas)/)']
