ad5d6299828f7d92f24e1f8984d3be8485c07f62
Dominic Hawton <hello@dominichawton.com>
2021-06-19 16:47:40 +1000

    refactor: :wrench: update jest config to account for baseUrl/aliases

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,9 @@ module.exports = {
   setupFilesAfterEnv: ['./jest.setup.js'],
   testEnvironment: 'jsdom',
   moduleNameMapper: {
-    '^components(.*)$': '<rootDir>/components$1'
+    '^components(.*)$': '<rootDir>/components$1',
+    '^lib(.*)$': '<rootDir>/lib$1',
+    '^theme(.*)$': '<rootDir>/theme$1'
   },
   transformIgnorePatterns: ['node_modules/(?!(lightweight-charts|fancy-canvas)/)']
 }
