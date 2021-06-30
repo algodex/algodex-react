523d3032066fe5d15d60c2854f7e8bb78a15adea
Dominic Hawton <hello@dominichawton.com>
2021-06-30 21:42:56 +1000

    fix(mobile): :fire: remove redundant test and story

--- components/mobile-order-book-view/mobile-order-book-view.spec.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import renderer from 'react-test-renderer'
-import MobileOrderBook from '.'
-
-it('MobileOrderBook: default', () => {
-  const component = renderer.create(< MobileOrderBook />)
-  const tree = component.toJSON()
-  expect(tree).toMatchSnapshot()
-})
--- components/mobile-order-book-view/mobile-order-book-view.stories.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import { storiesOf } from '@storybook/react'
-import { action } from '@storybook/addon-actions'
-import { linkTo } from '@storybook/addon-links'
-import MobileOrderBookView from '.'
-
-storiesOf('MobileOrderBookView', module).add('default', () => <MobileOrderBook />)
