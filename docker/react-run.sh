11e767176b443e0176fd9fc54c26544c7efa9cf8
Patrick Bennett <patrick+git@bennettfamily.org>
2021-07-24 16:00:45 -0400

    Get local services all running with passthrough bind volumes so all local editing goes into containers. Deployed containers will need to be redefined though.

--- docker/react-run.sh
+++ docker/react-run.sh
@@ -3,7 +3,7 @@ set -ex
 cd /repos/algodex-sdk
 yarn link
 cd /app
-rm -rf node_modules/@algodex
-yarn link @algodex/algodex-sdk
+rm -rf "node_modules/@algodex"
+yarn link "@algodex/algodex-sdk"
 yarn
 yarn dev
