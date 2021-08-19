12c20adaca57f8cd4a1ffba5f5d9887f02d1c2cb
Doug Richar <drichar@gmail.com>
2021-08-19 03:24:19 -0400

    fix: :bug: fix isLoading check on trade page

--- pages/trade/[id].js
+++ pages/trade/[id].js
@@ -129,7 +129,7 @@ export default function Home() {
 
   const renderDashboard = () => {
     const isError = assetQuery.isError || orderBookQuery.isError
-    const isLoading = assetQuery.isLoading || orderBookQuery.isLoading
+    const isLoading = assetQuery.isLoading || orderBookQuery.isLoading || !asset?.id
 
     if (isLoading) {
       return (
