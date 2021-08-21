916849c7938c581a0d4c06040199080c740595cf
Doug Richar <drichar@gmail.com>
2021-08-21 03:51:09 -0400

    feat(asset info): :sparkles: update asset info URL regex
    Update the asset info URL regex to consider domain extensions up to 7 characters (https://yieldly.finance was being rejected)

--- components/asset-info/index.jsx
+++ components/asset-info/index.jsx
@@ -37,7 +37,7 @@ export default function AssetInfo() {
 
   const renderLink = () => {
     const expression =
-      /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/
+      /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,7}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/
     const regex = new RegExp(expression)
 
     if (asset.info.url && regex.test(asset.info.url)) {
