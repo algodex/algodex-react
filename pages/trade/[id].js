9902bf24a178d0e5fc2ed2c572affd14d9ec8aef
Alexander Trefonas <atrefonas@gmail.com>
2021-08-26 07:12:27 -0400

    Change hasGateAccess to true

--- pages/trade/[id].js
+++ pages/trade/[id].js
@@ -163,7 +163,7 @@ export async function getServerSideProps({ req, res, query }) {
   const VERCEL_URL = process.env.NEXT_PUBLIC_VERCEL_URL
   const TESTNET_DOMAIN = process.env.NEXT_PUBLIC_TESTNET_DOMAIN
 
-  const hasGateAccess = await checkTestnetAccess(query?.loginKey || cookies.get('loginKey'))
+  const hasGateAccess = true; //await checkTestnetAccess(query?.loginKey || cookies.get('loginKey'))
 
   if (!hasGateAccess) {
     return {
