a9c5e6bf73f113d23fe11457dc1c872898aefdce
Alexander Trefonas <atrefonas@gmail.com>
2021-08-28 14:31:52 -0400

    Add sentry token

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -2,15 +2,14 @@
 // The config you add here will be used whenever a page is visited.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-//import * as Sentry from '@sentry/nextjs'
+import * as Sentry from '@sentry/nextjs'
 
-//const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN
+const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN
 
 // Sentry events should never be captured/sent in local development.
 // This marks them as such, just in case.
-//const ENVIRONMENT =
-//  process.env.NEXT_PUBLIC_ENV === 'development' ? 'development' : process.env.NEXT_PUBLIC_SENTRY_ENV
-/*
+const ENVIRONMENT =
+  process.env.NEXT_PUBLIC_ENV === 'development' ? 'development' : process.env.NEXT_PUBLIC_SENTRY_ENV
 
 Sentry.init({
   dsn: SENTRY_DSN || 'https://1573deb1875c4a53a47ee128a24c042f@o861560.ingest.sentry.io/5841341',
@@ -23,4 +22,3 @@ Sentry.init({
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
   // that it will also get attached to your source maps
 })
-*/
