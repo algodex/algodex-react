6e7aaec8b553b78a6ca25795995a39cd34a79730
Dominic Ma <dominic.c.ma@gmail.com>
2021-09-16 19:12:55 -0700

    attempt to fix for ios

--- store/use-store.js
+++ store/use-store.js
@@ -4,6 +4,23 @@ import produce from 'immer'
 import defaultLang from 'lang/en.js'
 import Big from 'big.js'
 
+export const roundValue = (value, decimalLimit) => {
+  console.log('rounding', value)
+  if (value === '' || value.slice(-1) === '0') {
+    return value
+  }
+
+  const split = value.toString().split(".");
+  const hasDecimals = split.length > 1;
+
+  if (hasDecimals && split[1].length >= decimalLimit) {
+    const rounded = new Big(value).round(decimalLimit, Big.roundDown).toString()
+    console.log("rounded", rounded);
+    return rounded
+  }
+
+  return value;
+}
 
 const immer = (config) => (set, get, api) =>
   config(
@@ -75,9 +92,9 @@ export const useStore = create(
         const amountChanged = order.amount !== state.order.amount;
         const price = typeof order.price === 'undefined' ? state.order.price : order.price
         const amount = typeof order.amount === "undefined" ? state.order.amount : order.amount
-        order.price = priceChanged ? new Big(price || 0).round(6).toString() : state.order.price;
-        order.amount = amountChanged ? new Big(amount || 0).round(state.asset.decimals, Big.roundDown).toString() : state.order.amount;
-        order.total = priceChanged || amountChanged ? new Big(order.price).times(new Big(order.amount)).round(6).toString() : state.order.total;
+        order.price = priceChanged ? roundValue(price, 6) : state.order.price;
+        order.amount = amountChanged ? roundValue(amount, state.asset.decimals) : state.order.amount;
+        order.total = priceChanged || amountChanged ? new Big(order.price || 0).times(new Big(order.amount || 0)).round(6).toString() : state.order.total;
 
         return {
           order: {
