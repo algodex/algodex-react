8b11d0d84284b8505bcae8c1673c4eff3c329e03
Dominic Ma <dominic.c.ma@gmail.com>
2021-10-10 20:02:57 -0700

    add order size bar and logic

--- components/order-options/index.jsx
+++ components/order-options/index.jsx
@@ -2,8 +2,11 @@ import { useState } from 'react'
 import PropTypes from 'prop-types'
 import { BodyCopyTiny, LabelSm } from 'components/type'
 import { ChevronDown } from 'react-feather'
-import { useRouter } from "next/router"
+import { useRouter } from 'next/router'
 import useTranslation from 'next-translate/useTranslation'
+
+import Icon from "components/icon"
+
 
 import {
   Container,
@@ -16,13 +19,15 @@ import {
   OptionsInput,
   OptionsButton
 } from './order-options.css'
+import InfoButton from 'components/info-button'
+import OrderSizeFilter from 'components/order-size-filter'
 
 function OrderOptions(props) {
-  const { order, onChange, allowTaker } = props
-  const { t } = useTranslation("place-order");
+  const { order, onChange, allowTaker, orderFilter, setOrderFilter } = props
+  const { t } = useTranslation('place-order')
 
-  const router = useRouter();
-  const showMakerOnly = router && router.query.showMakerOnly === "true";
+  const router = useRouter()
+  const showMakerOnly = router && router.query.showMakerOnly === 'true'
 
   const [isExpanded, setIsExpanded] = useState(false)
 
@@ -36,17 +41,16 @@ function OrderOptions(props) {
     }
   }
 
-
   const renderMessage = () => {
     switch (order.execution) {
       case 'maker':
         return `Your order will only execute as a maker order.`
 
       case 'taker':
-        return t("taker-only-desc")
+        return t('taker-only-desc')
 
       case 'both':
-        return t("maker-taker-desc")
+        return t('maker-taker-desc')
 
       default:
         return null
@@ -60,7 +64,7 @@ function OrderOptions(props) {
         onKeyDown={handleKeyDown}
         tabIndex="0"
       >
-        <LabelSm color="gray.500">{t("advanced-options")}</LabelSm>
+        <LabelSm color="gray.500">{t('advanced-options')}</LabelSm>
         <ArrowContainer>
           <ChevronDown />
         </ArrowContainer>
@@ -77,9 +81,9 @@ function OrderOptions(props) {
                 onChange={handleChange}
               />
               <OptionsButton as="label" htmlFor="order-both" size="small" type={order.type}>
-                {t("maker-taker")}
+                {t('maker-taker')}
               </OptionsButton>
-              
+
               {showMakerOnly && (
                 <>
                   <OptionsInput
@@ -103,12 +107,43 @@ function OrderOptions(props) {
                 onChange={handleChange}
               />
               <OptionsButton as="label" htmlFor="order-taker" size="small" type={order.type}>
-                {t("taker-only")}
+                {t('taker-only')}
               </OptionsButton>
             </OptionsWrapper>
             <BodyCopyTiny color="gray.500" textTransform="none">
               {renderMessage()}
             </BodyCopyTiny>
+
+            <div className="flex flex-row justify-between mt-5 align-middle">
+              <span className="text-sm font-semibold py-1">Order Size Filtering:</span>
+              <div className="relative">
+                <input 
+                className="bg-gray-900 h-7 w-14 rounded-sm border-2 border-gray-700 pr-5 text-right appearance-none" 
+                type="number"
+                placeholder="0"
+                min="0"
+                max="100"
+                step="1"
+                inputMode="numeric"
+                pattern="\d*"
+                value={orderFilter}
+                onChange={e => setOrderFilter(e.target.value)}
+                ></input>
+                <label className="absolute right-2 top-1"><Icon use="algoLogo" size={0.625} /></label>
+              </div>
+            </div>
+            <span className="text-xs"><InfoButton className="inline fill-current text-gray-500 " size={12} /> See FAQ</span>
+
+            <div className="pt-5 flex justify-between text-gray-500 text-sm">
+              <span class="block align-middle">0<Icon use="algoLogo" size={0.625} className="ml-1"></Icon></span>
+              <span class="block align-middle">100<Icon use="algoLogo" size={0.625} className="ml-1"></Icon></span>  
+            </div>
+            <OrderSizeFilter value={orderFilter} onChange={setOrderFilter}></OrderSizeFilter>
+            <div className="flex justify-between text-gray-500 text-sm">
+              <span class="block">Less Lag</span>
+              <span class="block">Better Execution</span>  
+            </div>
+
           </ExpandContent>
         </ExpandContentWrapper>
       </ExpandContainer>
--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,7 @@ import { createGlobalStyle, ThemeProvider } from 'styled-components'
 import { Toaster } from 'react-hot-toast'
 import theme from 'theme'
 import ReactGA from 'react-ga'
+import "tailwindcss/tailwind.css"
 
 const GlobalStyle = createGlobalStyle`
   html, body, div, span, applet, object, iframe,
@@ -85,6 +86,12 @@ const GlobalStyle = createGlobalStyle`
   }
   ::-webkit-scrollbar-corner {
     background: ${theme.colors.gray[700]};
+  }
+
+  input[type=number]::-webkit-inner-spin-button, 
+  input[type=number]::-webkit-outer-spin-button { 
+    -webkit-appearance: none; 
+    margin: 0; 
   }
 `
 
