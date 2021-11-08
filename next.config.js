31f6295a55d0aed1342043750a75c40ef30db3ea
Michael Feher <git@phearzero.com>
2021-11-07 20:41:54 -0600

    ⬆ Update @algodex packages

--- next.config.js
+++ next.config.js
@@ -3,14 +3,10 @@
 // https://nextjs.org/docs/api-reference/next.config.js/introduction
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-const { withSentryConfig } = require('@sentry/nextjs')
-const nextTranslate = require('next-translate')
+// const { withSentryConfig } = require('@sentry/nextjs')
+// const nextTranslate = require('next-translate')
 
 const moduleExports = {
-  extends: [
-    //...
-    'plugin:@next/next/recommended'
-  ]
   // ...nextTranslate(),
   // i18n: undefined,
   // async redirects() {
@@ -29,17 +25,17 @@ const moduleExports = {
   // }
 }
 
-const SentryWebpackPluginOptions = {
-  // Additional config options for the Sentry Webpack plugin. Keep in mind that
-  // the following options are set automatically, and overriding them is not
-  // recommended:
-  //   release, url, org, project, authToken, configFile, stripPrefix,
-  //   urlPrefix, include, ignore
-
-  silent: true // Suppresses all logs
-  // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options.
-}
+// const SentryWebpackPluginOptions = {
+//   // Additional config options for the Sentry Webpack plugin. Keep in mind that
+//   // the following options are set automatically, and overriding them is not
+//   // recommended:
+//   //   release, url, org, project, authToken, configFile, stripPrefix,
+//   //   urlPrefix, include, ignore
+//
+//   silent: true // Suppresses all logs
+//   // For all available options, see:
+//   // https://github.com/getsentry/sentry-webpack-plugin#options.
+// }
 
 // Make sure adding Sentry options is the last code to run before exporting, to
 // ensure that your source maps include changes from all other Webpack plugins
