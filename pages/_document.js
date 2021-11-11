c0932b2864874b72050e0c871ffea7140a8b033f
Michael Feher <git@phearzero.com>
2021-11-11 01:54:36 -0600

    🔧 Adds Meta to document

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,9 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
+const APP_NAME = 'Algodex'
+const APP_DESCRIPTION = 'Decentralized Exchange'
+
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet()
@@ -36,8 +39,17 @@ export default class MyDocument extends Document {
             rel="stylesheet"
             href="https://fonts.googleapis.com/css2?family=Roboto+Mono&family=Open+Sans+Condensed:wght@700&display=swap"
           />
+          <meta name="application-name" content={APP_NAME} />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+          <meta name="apple-mobile-web-app-title" content={APP_NAME} />
+          <meta name="description" content={APP_DESCRIPTION} />
+          <meta name="format-detection" content="telephone=no" />
+          <meta name="mobile-web-app-capable" content="yes" />
+          <meta name="theme-color" content="#4A5568" />
           <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
           <link rel="stylesheet" href="/fonts/style.css" />
+          <link rel="manifest" href="/manifest.json" />
           <link rel="icon" href="/favicon.ico" />
         </Head>
         <body>
