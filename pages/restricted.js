6e663bfc2545b0a2223fbc7f260192891819cc77
Dominic Ma <dominic.c.ma@gmail.com>
2021-11-10 21:32:21 -0800

    update restricted page

--- pages/restricted.js
+++ pages/restricted.js
@@ -1,29 +1,46 @@
 import styled from 'styled-components'
-import { HeaderSm, BodyCopySm } from 'components/type'
+import Head from 'next/head'
+import Header from 'components/header'
+import Icon from 'components/icon'
+import { AlertTriangle } from 'react-feather'
+// import { HeaderSm, BodyCopySm } from 'components/type'
 
-export const Container = styled.div`
-  position: relative;
-  flex: 1 1 0%;
-  display: flex;
-  height: 100vh;
-  width: 100vw;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  padding: 1.25rem 1.125rem;
-  text-align: center;
+export const Main = styled.div`
+  @media only screen and (min-width: 768px) {
+    background-image: url('/unauthorized.png');
+    background-size: 100% auto;
+    background-repeat: no-repeat;
+  }
 `
+
+const Modal = (props) => {
+  return <div className="absolute align-middle"></div>
+}
 
 const Restricted = () => {
   return (
-    <Container>
-      <HeaderSm color="gray.100" m={0} mb={16}>
-        Welcome to the party!
-      </HeaderSm>
-      <BodyCopySm color="gray.500" m={0}>
-        Oh... sorry. You&apos;re not invited. :/
-      </BodyCopySm>
-    </Container>
+    <div className="h-screen flex flex-col items-center">
+      <Header />
+
+      <Main className="flex flex-1 md:h-100 w-full items-center justify-center">
+        <div className="h-60 w-6/12 max-w-screen-md bg-gray-600 rounded pt-5">
+          <div className="p-3 flex h-full bg-gray-900 ">
+            <div className="w-3/12 flex justify-center items-center">
+              <AlertTriangle className="object-center" width={50} height={50} />
+            </div>
+            <div className="flex-1 pr-5">
+              <h1 className="text-gray-300 py-5"> At this time, Algodex is not available in your region (USA).</h1>
+              <h2 className="text-gray-300 leading-5">
+                If you wish to try the platform, you can use Algodex Testnet. Access to the Algodex
+                Testnet is simulated trading with testnet tokens for no monetary value and is not
+                intended as investment advice or a solicitation to engage in any type of trading
+                activity
+              </h2>
+            </div>
+          </div>
+        </div>
+      </Main>
+    </div>
   )
 }
 
