628d5bfa1b9e461c6714cdd82cdcd7dcb78c39e6
Dominic Ma <dominic.c.ma@gmail.com>
2021-11-17 22:57:50 -0800

    fix mobile and desktop styling for restricted page with email form

--- pages/restricted.js
+++ pages/restricted.js
@@ -1,45 +1,60 @@
-import styled from 'styled-components'
-import Head from 'next/head'
 import Header from 'components/header'
-import Icon from 'components/icon'
 import { AlertTriangle } from 'react-feather'
-// import { HeaderSm, BodyCopySm } from 'components/type'
+import { useRouter } from 'next/router'
 
-export const Main = styled.div`
-  @media only screen and (min-width: 768px) {
-    background-image: url('/unauthorized.png');
-    background-size: 100% auto;
-    background-repeat: no-repeat;
-  }
-`
 
 const Modal = (props) => {
-  return <div className="absolute align-middle"></div>
+  return (
+    <div className="h-auto w-10/12 md:w-8/12 max-w-screen-lg bg-gray-600 rounded-sm pt-5" {...props}>
+      {props.children}
+    </div>
+  )
+}
+
+const Input = (props) => {
+  return (
+    <input
+      className="w-11/12 my-8 px-4 h-8 tracking-wider bg-gray-700 placeholder-gray-200 border-gray-600 border border-solid"
+      {...props}
+    />
+  )
 }
 
 const Restricted = () => {
+  const router = useRouter()
+  const showEmail = router.query.showEmailForm
   return (
-    <div className="h-screen flex flex-col items-center">
+    <div className="h-screen flex flex-col items-center text-gray-200 font-semibold leading-5 tracking-wider">
       <Header />
 
-      <Main className="flex flex-1 md:h-100 w-full items-center justify-center">
-        <div className="h-60 w-6/12 max-w-screen-md bg-gray-600 rounded pt-5">
-          <div className="p-3 flex h-full bg-gray-900 ">
-            <div className="w-3/12 flex justify-center items-center">
+      <main className="flex flex-col flex-1 md:h-100 w-full items-center justify-center bg-cover bg-no-repeat bg-unauthorized-mobile md:bg-unauthorized">
+        <Modal>
+          <div className="flex flex-col md:flex-row text-center md:text-left h-full bg-gray-700 ">
+            <div className="mt-4 md:mt-0 mx-8 md:mx-12 flex justify-center items-center ">
               <AlertTriangle className="object-center" width={50} height={50} />
             </div>
-            <div className="flex-1 pr-5">
-              <h1 className="text-gray-300 py-5"> At this time, Algodex is not available in your region (USA).</h1>
-              <h2 className="text-gray-300 leading-5">
-                If you wish to try the platform, you can use Algodex Testnet. Access to the Algodex
-                Testnet is simulated trading with testnet tokens for no monetary value and is not
-                intended as investment advice or a solicitation to engage in any type of trading
-                activity
-              </h2>
+            <div className="mx-8 mb-8 flex-1 md:m-4 md:ml-0">
+              <p className="my-4">At this time, Algodex is not available in your region (USA).</p>
+              <p className="my-4">
+                If you wish to try the platform, you can use{' '}
+                <a href="//testnet.algodex.com" className="text-blue-400">
+                  Algodex Testnet
+                </a>
+                . Access to the Algodex Testnet is simulated trading with testnet tokens for no
+                monetary value and is not intended as investment advice or a solicitation to engage
+                in any type of trading activity.
+              </p>
             </div>
           </div>
-        </div>
-      </Main>
+        </Modal>
+        {showEmail && (
+          <div className="text-center text-xs m-6 mt-16 tracking-wide leading-normal">
+            <p>Algodex may be available in your region in the future!</p>
+            <p>If you wish to be notified, you can provide an email below.</p>
+            <Input name="email" type="email" placeholder="Email" />
+          </div>
+        )}
+      </main>
     </div>
   )
 }
