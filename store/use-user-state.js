d191895af330bce0ca1e677cf922d81dee677548
Michael Feher <git@phearzero.com>
2021-11-18 23:51:14 -0600

    📝 Document Localstorage State

--- store/use-user-state.js
+++ store/use-user-state.js
@@ -1,18 +1,73 @@
 import { createStore } from './use-store'
 import _ from 'lodash'
 const userState = (set, get) => ({
+  /**
+   * Assets should be a reduced list keyed by Asset ID
+   *
+   * This list is composed of a Algodex Assets Query and
+   */
   assets: {},
+  /**
+   * Add Assets
+   *
+   * Bulk update the Assets Store
+   * @param assets
+   */
+  addAssets: (assets) => {
+    set({
+      assets: assets.reduce((previous, asset) => {
+        // Add New Asset
+        if (typeof previous[asset.id] === 'undefined') {
+          previous[asset.id] = asset
+        }
+        // Merge Asset
+        else {
+          previous[asset.id] = {
+            ...previous[asset.id],
+            ...asset
+          }
+        }
+        return previous
+      }, get().assets)
+    })
+  },
+  /**
+   * Adds an Asset
+   *
+   * Adds an asset to local storage
+   * @param asset
+   */
   addAsset: (asset) => {
     let assets = get().assets
-    if (typeof asset.id !== 'undefined' && typeof assets[asset.id] === 'undefined') {
-      assets[asset.id] = asset
+    if (typeof asset.id !== 'undefined') {
+      // Add New Asset
+      if (typeof assets[asset.id] === 'undefined') {
+        assets[asset.id] = asset
+      }
+      // Merge Asset
+      else {
+        assets[asset.id] = {
+          ...assets[asset.id],
+          ...asset
+        }
+      }
       set({ assets })
     }
   },
+  /**
+   * Last Known Search Query
+   */
   query: '',
+  /**
+   * Set the Last Known Query
+   * @param query
+   */
   setQuery: (query) => {
     set({ query })
   },
+  /**
+   * SearchTable State
+   */
   search: {
     sortBy: [
       {
@@ -21,6 +76,10 @@ const userState = (set, get) => ({
       }
     ]
   },
+  /**
+   * Set SearchTable State
+   * @param search
+   */
   setSearch: (search) =>
     set({
       search: {
@@ -28,7 +87,14 @@ const userState = (set, get) => ({
         ...search
       }
     }),
+  /**
+   * Wallet Accounts[Work in Progress]
+   */
   accounts: {},
+  /**
+   * Set Accounts [Work in Progress]
+   * @param accounts
+   */
   setAccounts: (accounts) =>
     set({
       accounts: _.merge(get().accounts, accounts)
