e61556ca48d5e98ea2bd5e7ca532a9225f0e8d9a
Michael Feher <git@phearzero.com>
2021-11-19 14:02:42 -0600

    🎨 Adds jest and chromatic badges

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,15 @@ module.exports = {
   setupFilesAfterEnv: ['./jest.setup.js'],
   moduleDirectories: ['node_modules', '<rootDir>'],
   testEnvironment: 'jsdom',
+  collectCoverage: true,
+  coverageThreshold: {
+    //TODO: Raise confidence
+    global: {
+      branches: 20
+    }
+  },
+  collectCoverageFrom: ['**/components/**/*.{js,jsx}', '!**/components/**/*.stories.{js,jsx}'],
+  coverageReporters: ['json-summary', 'text', 'lcov'],
   moduleNameMapper: {
     '^components(.*)$': '<rootDir>/components$1',
     '^lib(.*)$': '<rootDir>/lib$1',
