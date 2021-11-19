6ffb47cb123f6d595accc503b844fd9ba8e2aa45
Michael Feher <git@phearzero.com>
2021-11-19 13:30:37 -0600

    🔥 Remove default stories

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,15 +3,13 @@ const path = require('path')
 module.exports = {
   stories: [
     '../components/**/*.stories.@(js|jsx|ts|tsx)',
-    '../stories/**/*.stories.mdx',
-    '../stories/**/*.stories.@(js|jsx|ts|tsx)'
   ],
   addons: [
-    '@storybook/addon-links', 
-    '@storybook/addon-essentials', 
-    'storybook-addon-next-router', 
-    { 
-      name: '@storybook/addon-postcss', 
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    'storybook-addon-next-router',
+    {
+      name: '@storybook/addon-postcss',
       options: {
         postcssLoaderOptions: {
           implementation: require('postcss')
