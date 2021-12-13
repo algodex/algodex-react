104ae25807c0722d8ace189356c153a899392b93
Ikechi <kechieleazar@gmail.com>
2021-12-13 08:50:59 +0100

    WIp for extracting language selection component

---
+++ components/language-selection/index.jsx
@@ -0,0 +1,13 @@
+// import PropTypes from 'prop-types'
+
+const LanguageSelection = () => {
+  return <div></div>
+}
+
+LanguageSelection.propTypes = {}
+
+LanguageSelection.defaultProps = {}
+
+LanguageSelection.displayName = 'LanguageSelection'
+
+export default LanguageSelection
