c0a5539228752b5203613c9d7574bc089ce39aa5
Ikechi <kechieleazar@gmail.com>
2021-12-15 13:14:27 +0100

    Fixing tab navigation across language

--- components/main-layout/main-layout.css.js
+++ components/main-layout/main-layout.css.js
@@ -197,7 +197,9 @@ export const MobileMenuButton = styled(Button)`
   background-color: ${({ theme }) => theme.colors.gray['800']};
   padding: 0;
   border: 1px solid ${({ theme }) => theme.colors.gray['700']};
-  max-width: ${({ characterLength }) => (characterLength > 8 ? '5.5rem' : '5rem')};
+  max-width: ${({ characterLength }) => (characterLength > 8 ? '4rem' : '5rem')};
+  min-width: ${({ characterLength }) => (characterLength > 8 ? '3.5rem' : '4rem')};
+  font-size: ${({ characterLength }) => (characterLength > 6 ? '10px' : '0.875rem')};
   overflow-wrap: anywhere;
 `
 
