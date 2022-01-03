c216d39f78b2c4f7edbe662a93dbe3c09ea8ca01
Ikechi <kechieleazar@gmail.com>
2022-01-03 13:19:09 +0100

    WIP for Algorand wallet connect

--- components/wallet-connect-dropdown/index.jsx
+++ components/wallet-connect-dropdown/index.jsx
@@ -1,20 +1,22 @@
 import {} from './wallet-connect.css'
-import PropTypes from 'prop-types'
-import useMyAlgo from 'hooks/useMyAlgo'
 
 import DropdownBody from './DropdownBody'
 import DropdownFooter from './DropdownFooter'
 import DropdownHeader from './DropdownHeader'
+import PropTypes from 'prop-types'
+import useMyAlgo from 'hooks/useMyAlgo'
+import useWalletConnect from 'hooks/use-wallet-connect'
 
 const WalletConnectDropdown = ({ closeFn }) => {
   const { connect: onWalletConnect } = useMyAlgo()
+  const { walletConnect } = useWalletConnect()
 
   return (
     <div className="flex flex-col justify-between">
       <DropdownHeader closeFn={closeFn} />
       <DropdownBody
         connectMyAlgoWallet={() => onWalletConnect()}
-        connectAlgorandMobileWallet={() => console.log('Connect Algorand Mobile Wallet')}
+        connectAlgorandMobileWallet={() => walletConnect()}
         closeFn={closeFn}
       />
       <DropdownFooter />
---
+++ hooks/use-wallet-connect.js
@@ -0,0 +1,113 @@
+import QRCodeModal from 'algorand-walletconnect-qrcode-modal'
+import WalletConnect from '@walletconnect/client'
+import { useState } from 'react'
+
+const ERROR = {
+  FAILED_TO_INIT: 'Algorand Mobile Wallet failed to initialize.',
+  FAILED_TO_CONNECT: 'Algorand Mobile Wallet failed to connect.'
+}
+
+export default function useWalletConnect() {
+  const [walletConnectAddresses, setAddresses] = useState()
+  const [walletConnection, setWalletConnection] = useState(null)
+
+  const walletConnect = async () => {
+    try {
+      const bridge = 'https://bridge.walletconnect.org'
+
+      // create new connector
+      const connector = new WalletConnect({ bridge, qrcodeModal: QRCodeModal })
+      // Set Wallet connector
+      setWalletConnection({ connector })
+
+      // check if already connected
+      if (!connector.connected) {
+        // create new session
+        await connector.createSession()
+      }
+
+      // subscribe to events
+      await subscribeToEvents({ connector })
+    } catch (e) {
+      console.error(ERROR.FAILED_TO_CONNECT, e)
+    }
+  }
+  const subscribeToEvents = ({ connector }) => {
+    if (!connector) {
+      return
+    }
+    connector.on('session_update', async (error, payload) => {
+      console.log(payload, 'session update')
+      if (error) {
+        throw error
+      }
+      const { accounts } = payload.params[0]
+      onSessionUpdate(accounts, connector)
+    })
+
+    connector.on('connect', (error, payload) => {
+      // console.log(connector.on('connect'), payload)
+      console.log(payload, 'has payload here')
+      if (error) {
+        throw error
+      }
+      onConnect(payload, connector)
+    })
+
+    connector.on('disconnect', (error, payload) => {
+      console.log(payload, 'disconnect')
+      if (error) {
+        throw error
+      }
+      onDisconnect()
+    })
+
+    if (connector.connected) {
+      const { accounts } = connector
+      // const address = accounts[0]
+      // console.log(address, 'address')
+      setWalletConnection({ ...walletConnection, connector })
+
+      onSessionUpdate(accounts)
+    }
+    setWalletConnection({ ...walletConnection, connector })
+  }
+
+  const killSession = async () => {
+    const { connector } = walletConnection
+    if (connector) {
+      connector.killSession()
+    }
+    resetApp()
+  }
+
+  const onConnect = async (payload, connector) => {
+    const { accounts } = payload.params[0]
+    console
+    // const address = accounts[0]
+    setWalletConnection({ ...walletConnection, connector })
+
+    setAddresses(accounts[0])
+    // getAccountAssets();
+  }
+
+  const onDisconnect = async () => {
+    resetApp()
+    killSession()
+  }
+
+  const resetApp = async () => {
+    setWalletConnection(null)
+  }
+
+  const onSessionUpdate = async (accounts, connector) => {
+    const address = accounts[0]
+    console.log(connector, walletConnectAddresses, 'connector')
+    await setWalletConnection({ ...walletConnection, address })
+  }
+
+  return {
+    walletConnect,
+    walletConnection
+  }
+}
