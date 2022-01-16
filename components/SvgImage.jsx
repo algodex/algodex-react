f289177d7b057ac96e5d9500e5273424fc430585
Michael J Feher <github@phearzero.com>
2022-01-16 11:34:36 -0600

    ♻ Refactor SvgImage.jsx

--- components/SvgImage.jsx
+++ components/SvgImage.jsx
@@ -1,6 +1,8 @@
-import theme from 'theme'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import theme, { parseThemeColor } from '../../theme'
 
-export default {
+const IMAGES = {
   logoLg: {
     viewBox: '0 0 1024 165.69',
     markup: (
@@ -81,3 +83,50 @@ export default {
     )
   }
 }
+
+const Svg = styled.svg`
+  display: inline-block;
+  vertical-align: middle;
+  width: ${({ w }) => (w ? `${w}rem` : 'auto')};
+  height: ${({ h }) => (h ? `${h}rem` : 'auto')};
+  ${({ color }) => color && `color: ${parseThemeColor(color)}`};
+`
+
+/**
+ * Component for displaying SVG images from a collection of definitions
+ *
+ * If the image has multiple colors or custom styles, they must be defined in
+ * the SVG markup. Otherwise, the color can be set via the `color` prop by
+ * defining all elements' `fill` and/or `stroke` attributes to "currentColor".
+ * The parent <svg/> color style will be set to the value of `color` and all
+ * child elements will inherit that color.
+ *
+ * @param {Object} props
+ * @param {String} props.use - Key of image to use
+ * @param {Number} props.w - Display width in rem units ('auto' if undefined)
+ * @param {String} props.h - Display height in rem units ('auto' if undefined)
+ * @param {String} props.color - A theme color in dot notation, e.g. 'gray.000'
+ * @deprecated
+ * @example <SvgImage use="myAlgo" w={3} color="gray.000" />
+ */
+function SvgImage(props) {
+  if (!IMAGES[props.use]) {
+    return null
+  }
+
+  const { viewBox, markup } = IMAGES[props.use]
+
+  return (
+    <Svg viewBox={viewBox} {...props}>
+      {markup}
+    </Svg>
+  )
+}
+
+SvgImage.propTypes = {
+  use: PropTypes.string.isRequired,
+  w: PropTypes.number,
+  h: PropTypes.number
+}
+
+export default SvgImage
