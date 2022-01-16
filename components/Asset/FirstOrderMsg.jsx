653ab083bcbbf013866010896451e951d21ad170
Michael J Feher <github@phearzero.com>
2022-01-16 12:47:29 -0600

    ♻ Refactor 🚧

--- components/Asset/FirstOrderMsg.jsx
+++ components/Asset/FirstOrderMsg.jsx
@@ -1,9 +1,54 @@
 import PropTypes from 'prop-types'
-import { HeaderSm, BodyCopySm } from 'components/type'
-import SvgImage from 'components/svg-image'
+import { HeaderSm, BodyCopySm } from 'components/Typography'
+import SvgImage from 'components/SvgImage'
+import styled from 'styled-components'
+export const Container = styled.div`
+  flex: 1 1 0%;
+  display: flex;
+  flex-direction: column;
+  overflow: hidden;
+  background-color: ${({ theme }) => theme.colors.background.dark};
+  padding: 0.875rem 0 1rem;
+`
 
-import { Container, EmptyState, Arrow, PairSlash } from './first-order-msg.css'
+export const EmptyState = styled.div`
+  position: relative;
+  flex: 1 1 0%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 1.25rem 1.125rem;
+  text-align: center;
+`
 
+export const Arrow = styled.div`
+  display: none;
+  position: absolute;
+  top: 50%;
+  right: 2.5rem;
+  transform: translateY(4.5rem);
+
+  svg {
+    transform: scale(-1) scaleY(-1) rotate(-115deg);
+  }
+
+  @media (min-width: 1024px) {
+    display: block;
+  }
+
+  @media (min-width: 1024px) and (orientation: portrait) {
+    display: none;
+  }
+
+  @media (min-width: 1536px) {
+    display: block;
+  }
+`
+
+export const PairSlash = styled.span`
+  letter-spacing: 0.125rem;
+`
 function FirstOrderMsg(props) {
   const { asset, isSignedIn } = props
 
