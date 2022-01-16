f1691ff86911195391531cad001e7a544a50253e
Michael J Feher <github@phearzero.com>
2022-01-16 11:45:26 -0600

    ♻ Refactor AlertIcon.jsx

---
+++ components/icon/AlertIcon.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+import { AlertTriangle } from 'react-feather'
+import { parseThemeColor } from '../../theme'
+
+export const AlertIcon = styled(AlertTriangle)`
+  stroke: ${({ color }) => parseThemeColor(color)};
+  width: ${({ size }) => `${size}rem`};
+  height: ${({ size }) => `${size}rem`};
+  margin: ${({ flex }) => (flex ? '0' : '0 0.5rem 0 0')};
+`
+
+export default AlertIcon
