e69f2ae54a7a47b32473acae2cc467c148b7cdc4
Ikechi <kechieleazar@gmail.com>
2022-01-18 12:37:20 +0100

    Variables for search bar with tables

--- components/Nav/SearchSidebar/SearchSidebar.stories.js
+++ components/Nav/SearchSidebar/SearchSidebar.stories.js
@@ -10,7 +10,7 @@ export default {
 
 const SearchWithTableTemplate = ({ ...args }) => (
   <div>
-    <Component />
+    <Component {...args} />
     <NavSearchTable />
   </div>
 )
@@ -20,6 +20,19 @@ const Template = (args) => <Component {...args} />
 export const Default = Template.bind({})
 
 export const WithSearchTable = SearchWithTableTemplate.bind({})
+
+WithSearchTable.args = {
+  searchHeight: '51px',
+  isActive: true,
+  onAssetFocus: false,
+  onAssetLeave: false,
+  onAssetClick: () => console.log('Hello'),
+  assets: [],
+  isListingVerifiedAssets: false,
+  algoPrice: '1.44',
+  isFilteringByFavorites: false,
+  setIsFilteringByFavorites: () => console.log('Hello')
+}
 
 Default.args = {
   assetPrice: 100,
