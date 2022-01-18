be1e1493ffb299057de76a884b29529a8d253889
Ikechi <kechieleazar@gmail.com>
2022-01-18 12:13:42 +0100

    Add search table types

--- components/Nav/SearchSidebar/SearchSidebar.stories.js
+++ components/Nav/SearchSidebar/SearchSidebar.stories.js
@@ -1,15 +1,25 @@
 // SearchSidebar.stories.js
 import { NavSearchSidebar as Component } from './SearchSidebar'
-import { NavSearchTable } from './SearchTable'
+// import NavSearchTable from './SearchTable'
+import NavSearchTable from './SearchTable'
 
 export default {
   title: 'Nav/SearchSidebar',
   component: Component
 }
 
+const SearchWithTableTemplate = ({ ...args }) => (
+  <div>
+    <Component />
+    <NavSearchTable />
+  </div>
+)
+
 const Template = (args) => <Component {...args} />
 
 export const Default = Template.bind({})
+
+export const WithSearchTable = SearchWithTableTemplate.bind({})
 
 Default.args = {
   assetPrice: 100,
