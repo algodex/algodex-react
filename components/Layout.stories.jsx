2b77b4bab529d1ccc5559e14cea7c493c59df238
Michael J Feher <github@phearzero.com>
2022-01-18 18:32:43 -0600

    📚 Adds multiple component example

--- components/Layout.stories.jsx
+++ components/Layout.stories.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
 import styled from 'styled-components'
 import Component from './Layout'
-
 import { Section } from 'components/Section'
 
+const asset = {
+  id: 15322902,
+  deleted: false,
+  txid: 'NOFSUK4EXHFFXJK3ZA6DZMGE6CAGQ7G5JT2X7FYTYQBSQEBZHY4Q',
+  timestamp: 1618666459,
+  decimals: 6,
+  name: 'LAMP',
+  txns: 377155,
+  fullName: 'Lamps',
+  circulating: 99989339745,
+  verified: false,
+  url: null,
+  total: 100000000000
+}
+const components = {
+  TradeLayout: {
+    Sidebar: (props) => <Section {...props}>Sidebar</Section>,
+    Footer: (props) => <Section {...props}>Footer</Section>,
+    Content: Section,
+    Controls: (props) => <Section {...props}>Controls</Section>
+  },
+  AnotherLayout: {
+    Sidebar: (props) => <Section {...props}>Sidebar</Section>,
+    Footer: (props) => <Section {...props}>Footer</Section>,
+    Content: (props) => <Section {...props}>Another Layout</Section>,
+    Controls: (props) => <Section {...props}>Controls</Section>
+  }
+}
 const Container = styled.div`
   width: 100vw;
   height: 100vh;
@@ -18,6 +45,13 @@ export default {
   title: '@algodex/components',
   component: Component,
   argTypes: {
+    components: {
+      options: Object.keys(components),
+      mapping: components,
+      control: {
+        type: 'select'
+      }
+    },
     rowHeight: {
       control: { type: 'range', min: 0, max: 100 }
     }
@@ -37,85 +71,17 @@ export const Layout = Template.bind({})
 Layout.parameters = {
   layout: 'fullscreen',
   controls: {
-    include: [
-      'rowHeight',
-      'sidebarCollapsed',
-      'sidebarExpanded',
-      'controlsCollapsed',
-      'controlsExpanded',
-      'footerCollapsed'
-    ]
+    exclude: ['asset']
   }
 }
 Layout.args = {
-  asset: {
-    id: 15322902,
-    deleted: false,
-    txid: 'NOFSUK4EXHFFXJK3ZA6DZMGE6CAGQ7G5JT2X7FYTYQBSQEBZHY4Q',
-    timestamp: 1618666459,
-    decimals: 6,
-    name: 'LAMP',
-    txns: 377155,
-    fullName: 'Lamps',
-    circulating: 99989339745,
-    verified: false,
-    url: null,
-    total: 100000000000
-  },
+  asset,
   rowHeight: 70,
   sidebarCollapsed: false,
   sidebarExpanded: false,
   controlsCollapsed: false,
   controlsExpanded: true,
   footerCollapsed: false,
-
-  components: {
-    Sidebar: (props) => <Section {...props}>Sidebar</Section>,
-    Footer: (props) => <Section {...props}>Footer</Section>,
-    Content: (props) => <Section {...props}>Content</Section>,
-    Controls: (props) => <Section {...props}>Controls</Section>
-  }
-}
-export const MobileLayout = Template.bind({})
-MobileLayout.parameters = {
-  layout: 'fullscreen',
-  controls: {
-    include: [
-      'rowHeight',
-      'sidebarCollapsed',
-      'sidebarExpanded',
-      'controlsCollapsed',
-      'controlsExpanded',
-      'footerCollapsed'
-    ]
-  }
-}
-MobileLayout.args = {
-  asset: {
-    id: 15322902,
-    deleted: false,
-    txid: 'NOFSUK4EXHFFXJK3ZA6DZMGE6CAGQ7G5JT2X7FYTYQBSQEBZHY4Q',
-    timestamp: 1618666459,
-    decimals: 6,
-    name: 'LAMP',
-    txns: 377155,
-    fullName: 'Lamps',
-    circulating: 99989339745,
-    verified: false,
-    url: null,
-    total: 100000000000
-  },
-  rowHeight: 85,
-  sidebarCollapsed: true,
-  sidebarExpanded: false,
-  controlsCollapsed: true,
-  controlsExpanded: true,
-  footerCollapsed: false,
-
-  components: {
-    Sidebar: (props) => <Section {...props}>Sidebar</Section>,
-    Footer: (props) => <Section {...props}>Footer</Section>,
-    Content: (props) => <Section {...props}>Content</Section>,
-    Controls: (props) => <Section {...props}>Controls</Section>
-  }
+  children: 'Storybook Time',
+  components: components.TradeLayout
 }
