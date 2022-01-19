9df90d915f622eb69cc9d6e1dfc0acc1e49a0cfe
Michael J Feher <github@phearzero.com>
2022-01-19 12:59:59 -0600

    📚 Trade History Stories

--- components/Asset/TradeHistory/TradeHistory.stories.js
+++ components/Asset/TradeHistory/TradeHistory.stories.js
@@ -1,29 +1,74 @@
 import React from 'react'
-import TradeHistoryView from '.'
+import { TradeHistoryView as Component, default as ComponentWithData } from './TradeHistory'
+import asset from 'spec/Asset'
+import styled from 'styled-components'
 
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  padding: 0;
+  margin: 0;
+  background: ${({ theme }) => theme.colors.gray['800']};
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+`
 export default {
-  title: 'Asset/Trade History',
-  component: TradeHistoryView,
+  title: 'Asset',
+  component: Component,
+  parameters: {
+    layout: 'fullscreen'
+  },
   decorators: [
     (Story) => (
-      <div
-        style={{
-          width: '320px',
-          height: '700px',
-          display: 'flex',
-          flexDirection: 'column'
-        }}
-      >
+      <Container>
         <Story />
-      </div>
+      </Container>
     )
   ]
 }
 
-const Template = (args) => <TradeHistoryView {...args} />
+const Template = (args) => <Component {...args} />
+const TemplateWithData = (args) => <ComponentWithData {...args} />
 
-export const Default = Template.bind({})
-Default.args = {
-  asset: { id: 123, name: 'YLDY', decimals: 6 },
-  tradesData: []
+export const TradeHistory = Template.bind({})
+TradeHistory.args = {
+  asset: {
+    ...asset,
+    ...{
+      circulating: 99989339745,
+      decimals: 6,
+      deleted: false,
+      fullName: 'Lamps',
+      id: 15322902,
+      name: 'LAMP',
+      timestamp: 1618666459,
+      total: 100000000000,
+      txid: 'NOFSUK4EXHFFXJK3ZA6DZMGE6CAGQ7G5JT2X7FYTYQBSQEBZHY4Q',
+      txns: 377155,
+      url: null,
+      verified: false
+    }
+  },
+  tradesData: [
+    {
+      amount: '0.001171',
+      id: 341492,
+      price: '2490.0000',
+      timestamp: 1642618469000,
+      type: 'buyASA'
+    },
+    {
+      amount: '0.001171',
+      id: 341492,
+      price: '2490.0000',
+      timestamp: 1642618469000,
+      type: 'buyASA'
+    }
+  ]
+}
+
+export const TradeHistoryPreview = TemplateWithData.bind({})
+TradeHistoryPreview.args = {
+  asset
 }
