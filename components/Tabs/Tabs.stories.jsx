be94ee7e1f15699e382b2be347b3e3426bf524ec
Ikechi <kechieleazar@gmail.com>
2022-01-28 18:34:13 +0100

    refactor 🚧: Create story for tabs

---
+++ components/Tabs/Tabs.stories.jsx
@@ -0,0 +1,31 @@
+import { Tabs as Component } from './Tabs'
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: ${({ theme }) => theme.colors.gray['800']};
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  padding: 0;
+  margin: 0;
+`
+export default {
+  title: '@algodex/components/Tabs/Tabs',
+  component: Component,
+  parameters: { layout: 'fullscreen' },
+  decorators: [
+    (Story) => (
+      <Container>
+        <Story />
+      </Container>
+    )
+  ]
+}
+
+const Template = (args) => <Component {...args} />
+// const TemplateWithData = (args) => <ComponentWithData {...args} />
+export const Tabs = Template.bind({})
+Tabs.args = {}
