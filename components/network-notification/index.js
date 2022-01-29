cd4ad3a89152bf8db4d5f09a7c3a1ab78ea67ff4
Ikechi <kechieleazar@gmail.com>
2022-01-29 23:53:26 +0100

    Fix🐛: Hide intro modals by default

--- components/network-notification/index.js
+++ components/network-notification/index.js
@@ -1,5 +1,6 @@
 import NetworkBanner from './NetworkBanner'
 import NetworkNotificationModal from './NetworkNotificationModal'
+import { useEffect } from 'react'
 import { useUserStore } from '../../store'
 
 const NetworkHandler = () => {
@@ -8,11 +9,22 @@ const NetworkHandler = () => {
   const hasMainnetNotificationModal = useUserStore((state) => state.hasMainnetNotificationModal)
   const hasTestnetNotificationModal = useUserStore((state) => state.hasTestnetNotificationModal)
   const activeNetwork = useUserStore((state) => state.activeNetwork)
+  const setHasMainnetRibbon = useUserStore((state) => state.setHasMainnetRibbon)
+  const setHasTestnetRibbon = useUserStore((state) => state.setHasTestnetRibbon)
+  const setHasMainnetNotificationModal = useUserStore((state) => state.setHasMainnetNotificationModal)
+  const setHasTestnetNotificationModal = useUserStore((state) => state.setHasTestnetNotificationModal)
 
   const isRibbonActive = activeNetwork === 'testnet' ? hasTestnetRibbon : hasMainnetRibbon
   const isModalActive =
     activeNetwork === 'testnet' ? hasTestnetNotificationModal : hasMainnetNotificationModal
 
+  useEffect(() => {
+    hasMainnetRibbon === null && setHasMainnetRibbon(true)
+    hasTestnetRibbon === null && setHasTestnetRibbon(true)
+    hasMainnetNotificationModal === null && setHasMainnetNotificationModal(true)
+    hasTestnetNotificationModal === null && setHasTestnetNotificationModal(true)
+  }, [])
+  
   return (
     <div>
       {isRibbonActive && <NetworkBanner />}
