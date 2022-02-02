2b3dedf2101a3261c79a0011d6ee4a29c894150f
Michael J Feher <github@phearzero.com>
2022-02-01 21:57:16 -0600

    💚 Fix storybooks

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,21 @@
 const path = require('path');
+const fs = require('fs');
 
+function getPackageDir(filepath) {
+  let currDir = path.dirname(require.resolve(filepath));
+  while (true) {
+    if (fs.existsSync(path.join(currDir, "package.json"))) {
+      return currDir;
+    }
+    const { dir, root } = path.parse(currDir);
+    if (dir === root) {
+      throw new Error(
+          `Could not find package.json in the parent directories starting from ${filepath}.`
+      );
+    }
+    currDir = dir;
+  }
+}
 module.exports = {
   stories: ['../components/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials', 'storybook-addon-next-router', {
@@ -17,7 +33,10 @@ module.exports = {
       "@/hooks": path.resolve(__dirname, "../hooks"),
       "@/store": path.resolve(__dirname, "../store"),
       "@/services": path.resolve(__dirname, "../services"),
-      "@/utils": path.resolve(__dirname, "../utils")
+      "@/utils": path.resolve(__dirname, "../utils"),
+      "@emotion/core": getPackageDir("@emotion/react"),
+      "@emotion/styled": getPackageDir("@emotion/styled"),
+      "emotion-theming": getPackageDir("@emotion/react")
     };
     config.resolve.modules = [
         path.resolve(__dirname, '..'), 'node_modules',
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,11 +1,9 @@
 import React from 'react'
 import { jsxDecorator } from 'storybook-addon-jsx'
 import { RouterContext } from 'next/dist/shared/lib/router-context'
-// import { createGlobalStyle, ThemeProvider } from 'styled-components'
 import { ThemeProvider } from '@mui/material/styles';
 import { Global, css } from '@emotion/react';
 import CssBaseline from '@mui/material/CssBaseline';
-import { CacheProvider } from '@emotion/react';
 import { QueryClient, QueryClientProvider } from 'react-query'
 import theme from '../theme'
 import "tailwindcss/tailwind.css"
@@ -81,8 +79,8 @@ const base = css`
   body {
     box-sizing: border-box;
     font-family: 'Inter', -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-    /* background: ${theme.colors.background.dark}; */
-    color: ${theme.colors.gray['400']};
+    /* background: ${theme.palette.background.dark}; */
+    color: ${theme.palette.gray['400']};
   }
   *,
   *:before,
@@ -132,6 +130,7 @@ export const decorators = [
   ),
   (Story) => (
     <>
+        <CssBaseline />
       <Global styles={base} />
       {Story()}
     </>
