cedb0eb62ddc17a92d7cde1edd6bddc7ab80fc9b
Michael J Feher <github@phearzero.com>
2022-02-01 23:49:44 -0600

    💚 Fix chromatic

--- components/Wallet/PlaceOrder/Form.stories.jsx
+++ components/Wallet/PlaceOrder/Form.stories.jsx
@@ -1,63 +1,44 @@
 import React from 'react'
 import { PlaceOrderForm as Component } from './Form'
-import styled from '@emotion/styled'
-import asset from 'spec/Asset'
+import generateAsset from 'spec/Asset'
 
-const Container = styled.div`
-  width: 100vw;
-  height: 100vh;
-  background: ${({ theme }) => theme.palette.gray['800']};
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
-  padding: 0;
-  margin: 0;
-`
+const testAsset = generateAsset()
 export default {
   title: '@algodex/recipes/Wallet/Place Order Form',
   component: Component,
   parameters: { layout: 'fullscreen' },
-  decorators: [
-    (Story) => (
-      <Container>
-        <Story />
-      </Container>
-    )
-  ]
-}
-
-const Template = (args) => <Component {...args} />
-// const TemplateWithData = (args) => <ComponentWithData {...args} />
-delete asset.name
-export const PlaceOrderForm = Template.bind({})
-PlaceOrderForm.args = {
-  asset,
-  wallet: {
-    address: 'TJFFNUYWHPPIYDE4DGGYPGHWKGAPJEWP3DGE5THZS3B2M2XIAPQ2WY3X4I',
-    balance: 100,
-    assets: {
-      15322902: {
-        balance: 0
+  args: {
+    isRegenerate: false,
+    asset: testAsset,
+    wallet: {
+      address: 'TJFFNUYWHPPIYDE4DGGYPGHWKGAPJEWP3DGE5THZS3B2M2XIAPQ2WY3X4I',
+      balance: 100,
+      assets: {
+        [testAsset.id]: {
+          balance: 0
+        }
       }
+    },
+    onSubmit: (e) => {
+      let order = {
+        amount: e.target.amount.value,
+        type: e.target.type.value,
+        price: e.target.price.value,
+        total: e.target.total.value,
+        asset: e.target.asset.value
+        // execution: e.target.execution.value
+      }
+      console.log(order)
+      e.preventDefault()
     }
-  },
-  onSubmit: (e) => {
-    let order = {
-      amount: e.target.amount.value,
-      type: e.target.type.value,
-      price: e.target.price.value,
-      total: e.target.total.value,
-      asset: e.target.asset.value
-      // execution: e.target.execution.value
-    }
-    console.log(order)
-    e.preventDefault()
   }
 }
 
-// export const OpenOrdersPreview = TemplateWithData.bind({})
-// OpenOrdersPreview.args = {
-//     wallet: {
-//         address: 'TJFFNUYWHPPIYDE4DGGYPGHWKGAPJEWP3DGE5THZS3B2M2XIAPQ2WY3X4I'
-//     }
-// }
+/* eslint-disable */
+export const PlaceOrderForm = ({isRegenerate, asset, wallet, ...props}) => {
+  if (isRegenerate) {
+    asset = generateAsset()
+    wallet.assets[asset.id] = wallet.assets[testAsset.id]
+  }
+  return <Component asset={asset} wallet={wallet} {...props} />
+}
