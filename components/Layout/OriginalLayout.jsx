c523bb7f4bdfce26d0142fa3127f9b7cfaf84756
Michael J Feher <github@phearzero.com>
2022-02-14 15:11:39 -0600

    🐛 Update button import

--- components/Layout/OriginalLayout.jsx
+++ components/Layout/OriginalLayout.jsx
@@ -1,4 +1,4 @@
-import Button from 'components/button'
+import Button from '@/components/Button'
 import styled from '@emotion/styled'
 import detectMobileDisplay from '@/utils/detectMobileDisplay'
 const WalletSection = styled.section`
