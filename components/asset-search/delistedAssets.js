07e20707658e498914406e9532c1ccba65fce0f1
Treasurechic <babychic140@gmail.com>
2022-02-14 10:44:12 +0100

    :zap: delisted scam coins from the asset list

---
+++ components/asset-search/delistedAssets.js
@@ -0,0 +1,107 @@
+export const DelistedAssets = [
+  {
+    assetName: 'Algodex',
+    unitName: 'ALGX',
+    verified: false,
+    destroyed: false,
+    assetId: 57867695,
+    isTraded: true,
+    decimals: 10,
+    priceChg24Pct: 2.1186605106317757,
+    price: '0.000120500000',
+    formattedPrice: '1.205000',
+    hasOrders: true,
+    formattedASALiquidity: '140961.4845331884',
+    formattedAlgoLiquidity: '56470.725607'
+  },
+  {
+    assetName: 'mikasa',
+    unitName: 'mike',
+    verified: false,
+    destroyed: false,
+    assetId: 52773530,
+    isTraded: false,
+    hasOrders: false,
+    formattedASALiquidity: '0',
+    formattedAlgoLiquidity: '0.000000'
+  },
+  {
+    assetName: 'mikasa',
+    unitName: 'mike',
+    verified: false,
+    destroyed: false,
+    assetId: 52772474,
+    isTraded: false,
+    hasOrders: false,
+    formattedASALiquidity: '0',
+    formattedAlgoLiquidity: '0.000000'
+  },
+  {
+    assetName: 'mikasa',
+    unitName: 'mike',
+    verified: false,
+    destroyed: false,
+    assetId: 52771043,
+    isTraded: false,
+    hasOrders: false,
+    formattedASALiquidity: '0',
+    formattedAlgoLiquidity: '0.000000'
+  },
+  {
+    assetName: 'mikasa',
+    unitName: 'mike',
+    verified: false,
+    destroyed: false,
+    assetId: 52769853,
+    isTraded: false,
+    hasOrders: false,
+    formattedASALiquidity: '0',
+    formattedAlgoLiquidity: '0.000000'
+  },
+  {
+    assetName: 'mikasa',
+    unitName: 'mike',
+    verified: false,
+    destroyed: false,
+    assetId: 52769581,
+    isTraded: false,
+    hasOrders: false,
+    formattedASALiquidity: '0',
+    formattedAlgoLiquidity: '0.000000'
+  },
+  {
+    assetName: 'mikasa',
+    unitName: 'mike',
+    verified: false,
+    destroyed: false,
+    assetId: 52768943,
+    isTraded: false,
+    hasOrders: false,
+    formattedASALiquidity: '0',
+    formattedAlgoLiquidity: '0.000000'
+  },
+  {
+    assetName: 'JONAH',
+    unitName: 'MIKE',
+    verified: false,
+    destroyed: false,
+    assetId: 25340506,
+    isTraded: false,
+    decimals: 6,
+    hasOrders: false,
+    formattedASALiquidity: '0.000000',
+    formattedAlgoLiquidity: '0.000000'
+  },
+  {
+    assetName: 'Mike Token',
+    unitName: 'MKT',
+    verified: false,
+    destroyed: false,
+    assetId: 15175087,
+    isTraded: false,
+    decimals: 0,
+    hasOrders: false,
+    formattedASALiquidity: '0',
+    formattedAlgoLiquidity: '0.000000'
+  }
+]
