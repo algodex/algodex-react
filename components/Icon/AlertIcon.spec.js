113ce637913d3edfb0cb467cf212fcc91d8b9269
Ikechi <kechieleazar@gmail.com>
2022-02-22 18:18:07 +0100

    Update target for component

--- components/Icon/AlertIcon.spec.js
+++ components/Icon/AlertIcon.spec.js
@@ -5,14 +5,14 @@ import { render } from 'test/test-utils'
 expect.extend(matchers)
 describe('Icon Button', () => {
   it('Should render icon with the right props', () => {
-    const { queryByTestId } = render(<AlertIcon color="gray" flex={true} size={10} />)
-    expect(queryByTestId('info-icon-wrapper')).toHaveStyleRule('width', '10rem')
-    expect(queryByTestId('info-icon-wrapper')).toHaveStyleRule('height', '10rem')
-    expect(queryByTestId('info-icon-wrapper')).toHaveStyleRule('margin', '0')
+    const { queryByTestId } = render(<AlertIcon data-testid="alert-icon" color="gray" flex={true} size={10} />)
+    expect(queryByTestId('alert-icon')).toHaveStyleRule('width', '10rem')
+    expect(queryByTestId('alert-icon')).toHaveStyleRule('height', '10rem')
+    expect(queryByTestId('alert-icon')).toHaveStyleRule('margin', '0')
   })
 
   it('Should render icon diffently when flex is set to false', () => {
-    const { queryByTestId } = render(<AlertIcon color="gray" flex={false} size={10} />)
-    expect(queryByTestId('info-icon-wrapper')).toHaveStyleRule('margin', '0 0.5rem 0 0')
+    const { queryByTestId } = render(<AlertIcon data-testid="alert-icon" color="gray" flex={false} size={10} />)
+    expect(queryByTestId('alert-icon')).toHaveStyleRule('margin', '0 0.5rem 0 0')
   })
 })
