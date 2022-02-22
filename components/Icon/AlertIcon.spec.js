994d4beb0deb194d5fc4f26c0c120df7ad0ea201
Ikechi <kechieleazar@gmail.com>
2022-02-22 18:19:45 +0100

    Cleanup component

--- components/Icon/AlertIcon.spec.js
+++ components/Icon/AlertIcon.spec.js
@@ -5,14 +5,18 @@ import { render } from 'test/test-utils'
 expect.extend(matchers)
 describe('Icon Button', () => {
   it('Should render icon with the right props', () => {
-    const { queryByTestId } = render(<AlertIcon data-testid="alert-icon" color="gray" flex={true} size={10} />)
+    const { queryByTestId } = render(
+      <AlertIcon data-testid="alert-icon" color="gray" flex={true} size={10} />
+    )
     expect(queryByTestId('alert-icon')).toHaveStyleRule('width', '10rem')
     expect(queryByTestId('alert-icon')).toHaveStyleRule('height', '10rem')
     expect(queryByTestId('alert-icon')).toHaveStyleRule('margin', '0')
   })
 
   it('Should render icon diffently when flex is set to false', () => {
-    const { queryByTestId } = render(<AlertIcon data-testid="alert-icon" color="gray" flex={false} size={10} />)
+    const { queryByTestId } = render(
+      <AlertIcon data-testid="alert-icon" color="gray" flex={false} size={10} />
+    )
     expect(queryByTestId('alert-icon')).toHaveStyleRule('margin', '0 0.5rem 0 0')
   })
 })
