2c49b862c186a469cf10775748a0159ec6f31ba1
Treasurechic <babychic140@gmail.com>
2022-02-22 21:49:11 +0100

    :white_check_mark: Test for wallet

--- components/Wallet/WalletTabs.spec.js
+++ components/Wallet/WalletTabs.spec.js
@@ -1,51 +1,21 @@
-import React from 'react'
 import { render } from 'test/test-utils'
-import { Tab } from './WalletTabs'
+import { matchers } from '@emotion/jest'
+import WalletTabs from './WalletTabs'
 
-describe('Order Book', () => {
-  const OPEN_ORDERS_PANEL = 'open-orders'
-  const ORDER_HISTORY_PANEL = 'order-history'
-  const ASSETS_PANEL = 'assets'
-
+expect.extend(matchers)
+describe('Wallet Tabs', () => {
   it('should show open orders', () => {
-    const { queryByTestId } = render(
-      <Tab
-        data-testid="open-orders-tab"
-        isActive={selectedPanel === OPEN_ORDERS_PANEL}
-        onClick={() => setSelectedPanel(OPEN_ORDERS_PANEL)}
-      />
-    )
+    const { queryByTestId } = render(<WalletTabs initialPanel="open-orders" area="footer" />)
     expect(queryByTestId('open-orders-tab')).not.toBeNull()
-    expect(queryByTestId(OPEN_ORDERS_PANEL)).not.toBeNull()
-    expect(queryByTestId(ORDER_HISTORY_PANEL)).toBeNull()
-    expect(queryByTestId(ASSETS_PANEL)).toBeNull()
+    // expect(queryByTestId('order-history-tab')).toHaveAttribute('isActive', false)
   })
-
   it('should show order history', () => {
-    const { queryByTestId } = render(
-      <Tab
-        data-testid="order-history-tab"
-        isActive={selectedPanel === ORDER_HISTORY_PANEL}
-        onClick={() => setSelectedPanel(ORDER_HISTORY_PANEL)}
-      />
-    )
+    const { queryByTestId } = render(<WalletTabs initialPanel="order-history" area="footer" />)
     expect(queryByTestId('order-history-tab')).not.toBeNull()
-    expect(queryByTestId(ORDER_HISTORY_PANEL)).not.toBeNull()
-    expect(queryByTestId(OPEN_ORDERS_PANEL)).toBeNull()
-    expect(queryByTestId(ASSETS_PANEL)).toBeNull()
   })
 
   it('should show asset panel', () => {
-    const { queryByTestId } = render(
-      <Tab
-        data-testid="asset-tab"
-        isActive={selectedPanel === ASSETS_PANEL}
-        onClick={() => setSelectedPanel(ASSETS_PANEL)}
-      />
-    )
-    expect(queryByTestId('asset-tab')).not.toBeNull()
-    expect(queryByTestId(ASSETS_PANEL)).not.toBeNull()
-    expect(queryByTestId(OPEN_ORDERS_PANEL)).toBeNull()
-    expect(queryByTestId(ORDER_HISTORY_PANEL)).toBeNull()
+    const { queryByTestId } = render(<WalletTabs initialPanel="assets" area="footer" />)
+    expect(queryByTestId('assets-panel-tab')).not.toBeNull()
   })
 })
