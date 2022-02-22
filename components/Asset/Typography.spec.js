ec0281448486c2faf6cffcf05f0a228ada096c51
Treasurechic <babychic140@gmail.com>
2022-02-22 14:42:25 +0100

    :lipstick: Fix build error

--- components/Asset/Typography.spec.js
+++ components/Asset/Typography.spec.js
@@ -10,14 +10,14 @@ describe('AssetName', () => {
     const { queryByTestId } = render(
       <AssetName data-testid="AssetName-element">hello world</AssetName>
     )
-    expect(queryByTestId('AssetName-element')).toHaveStyle('letter-spacing', '0.025rem')
-    expect(queryByTestId('AssetName-element')).not.toHaveStyle('letter-spacing', '0')
+    expect(queryByTestId('AssetName-element')).toHaveStyle('letter-spacing: 0.025rem')
+    expect(queryByTestId('AssetName-element')).not.toHaveStyle('letter-spacing: 0')
   })
 })
 
 describe('AssetId', () => {
   it('Text should have a unique font size', () => {
     const { queryByTestId } = render(<AssetId data-testid="AssetId-element">#98848</AssetId>)
-    expect(queryByTestId('AssetId-element')).toHaveStyle('font-size', '0.625rem')
+    expect(queryByTestId('AssetId-element')).toHaveStyle('font-size: 0.625rem')
   })
 })
