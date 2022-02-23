a0f057de93969d8a4322aa5333bc92e731397b05
Treasurechic <babychic140@gmail.com>
2022-02-23 22:58:42 +0100

    :white_check_mark: Test for header

--- components/Nav/Header/header.spec.js
+++ components/Nav/Header/header.spec.js
@@ -3,13 +3,24 @@ import singletonRouter from 'next/router'
 import { render } from 'test/test-utils'
 import { Header } from './index'
 import { matchers } from '@emotion/jest'
+import { MobileNavigation } from './header.css'
 
+jest.mock('next/dist/client/router', () => require('next-router-mock'))
 expect.extend(matchers)
-
 describe('Header', () => {
   it('should render the container', () => {
-    const { queryByTestId } = render(<Header />)
-    expect(queryByTestId('header-container')).toHaveClass('flex')
+    const { queryByTestId } = render(<Header router={singletonRouter} />)
+    expect(queryByTestId('header-container')).toBeVisible()
     expect(queryByTestId('header-container')).not.toBeNull()
+    expect(queryByTestId('header-network-dropdown-element')).not.toBeNull()
+    expect(queryByTestId('header-navigation-element')).not.toBeNull()
+  })
+
+  it('should make mobile nav visible if isOpen is true', () => {
+    const { queryByTestId } = render(
+      <MobileNavigation data-testid="mobile-nav-element" isOpen={true} />
+    )
+    expect(queryByTestId('mobile-nav-element')).toBeVisible()
+    expect(queryByTestId('mobile-nav-element')).not.toBeNull()
   })
 })
