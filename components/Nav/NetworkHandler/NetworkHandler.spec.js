73d3d7d7821792cc9377ebed9c34192dfd448178
Ikechi <kechieleazar@gmail.com>
2022-02-25 22:07:43 +0100

    Complete test for Network modal

--- components/Nav/NetworkHandler/NetworkHandler.spec.js
+++ components/Nav/NetworkHandler/NetworkHandler.spec.js
@@ -1,20 +1,14 @@
 import { fireEvent, render } from 'test/test-utils'
 
 import NetworkBanner from './NetworkBanner'
-// import NetworkNotificationModal from './NetworkNotificationModal'
-// import NotificationModal from './NotificationModal'
+import NotificationModal from './NotificationModal'
 import { matchers } from '@emotion/jest'
 import useUserState from '@/store/use-user-state'
 
 expect.extend(matchers)
-jest.mock('next-translate/useTranslation', () => require('next-translate'))
-
-describe('Network Notification Modal Component', () => {
-  it('Should render ', () => {})
-})
 
 describe('Network Banner Component', () => {
-  it('Should render Banner', () => {
+  it('Should render Banner for Testnet', () => {
     function NetworkBannerTest() {
       const setHasTestnetRibbon = useUserState((state) => state.setHasTestnetRibbon)
       setHasTestnetRibbon(true)
@@ -23,9 +17,35 @@ describe('Network Banner Component', () => {
     const { queryByTestId } = render(<NetworkBannerTest />)
     expect(queryByTestId('banner-container')).not.toBeNull()
     fireEvent.click(queryByTestId('banner-close-btn'))
-    // expect(queryByTestId('banner-container')).toBeNull()
+    expect(queryByTestId('banner-container')).toBeNull()
+  })
+
+  it('Should render Banner for Mainnet', () => {
+    function NetworkBannerTest() {
+      const setActiveNetwork = useUserState((state) => state.setActiveNetwork)
+      const setHasMainnetRibbon = useUserState((state) => state.setHasMainnetRibbon)
+      setHasMainnetRibbon(true)
+      setActiveNetwork('mainnet')
+      return <NetworkBanner />
+    }
+    const { queryByTestId } = render(<NetworkBannerTest />)
+    expect(queryByTestId('banner-container')).not.toBeNull()
+    fireEvent.click(queryByTestId('banner-close-btn'))
+    expect(queryByTestId('banner-container')).toBeNull()
   })
 })
+
 describe('Notification Modal Component', () => {
-  it('Should render InfoFlyover Component', () => {})
+  it('Should render Network notification Modal', () => {
+    const { queryByTestId } = render(
+      <NotificationModal isModalActive={false} content={<div>Hello World</div>} />
+    )
+    expect(queryByTestId('notification-modal-wrapper')).toBeNull()
+  })
+  it('Should not render Network notification Modal', () => {
+    const { queryByTestId } = render(
+      <NotificationModal isModalActive={true} content={<div>Hello World</div>} />
+    )
+    expect(queryByTestId('notification-modal-wrapper')).not.toBeNull()
+  })
 })
