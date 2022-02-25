c07dffb42f18cbbb9277b5b7db1c3e6b721dcd8a
Treasurechic <babychic140@gmail.com>
2022-02-25 17:53:33 +0100

    :white_check_mark: Test for modal

--- components/Modal.spec.js
+++ components/Modal.spec.js
@@ -3,38 +3,27 @@ import { render } from 'test/test-utils'
 import ModalWrapper from './Modal'
 
 expect.extend(matchers)
-const element = <div></div>
 describe('ModalWrapper', () => {
   it('Should render modal with backdrop', () => {
     const { queryByTestId } = render(
-      <ModalWrapper
-        data-testid="ModalWrapper-element"
-        hideBackdrop={false}
-        isVisible={true}
-        children={element}
-      >
+      <ModalWrapper data-testid="ModalWrapper-element" hideBackdrop={false} isVisible={true}>
         <div data-testid="modal-element" style={{ backgroundColor: 'none' }}>
           Hello
         </div>
       </ModalWrapper>
     )
     expect(queryByTestId('modal-element')).not.toBeNull()
-    expect(queryByTestId('modal-element')).toHaveAttribute('style', { backgroundColor: 'none' })
+    expect(queryByTestId('modal-element')).toHaveStyle({ 'background-color': 'none' })
   })
 
   it('Should render modal without backdrop', () => {
     const { queryByTestId } = render(
-      <ModalWrapper
-        data-testid="ModalWrapper-element"
-        hideBackdrop={true}
-        isVisible={true}
-        children={element}
-      >
+      <ModalWrapper data-testid="ModalWrapper-element" hideBackdrop={true} isVisible={true}>
         <div data-testid="modal-element" style={{ backgroundColor: 'rgba(0, 0, 0, 0.7)' }}></div>
       </ModalWrapper>
     )
-    expect(queryByTestId('modal-element')).toHaveAttribute('style', {
-      backgroundColor: 'rgba(0, 0, 0, 0.7)'
+    expect(queryByTestId('modal-element')).toHaveStyle({
+      'background-color': 'rgba(0, 0, 0, 0.7)'
     })
   })
 })
