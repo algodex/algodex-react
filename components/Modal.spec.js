ae9af2e2e1a074a3ab2094ade584566aef7eb8d6
Treasurechic <babychic140@gmail.com>
2022-02-25 17:36:19 +0100

    :white_check_mark: Test for modal

--- components/Modal.spec.js
+++ components/Modal.spec.js
@@ -5,15 +5,20 @@ import ModalWrapper from './Modal'
 expect.extend(matchers)
 const element = <div></div>
 describe('ModalWrapper', () => {
-  it('Should render modal neatly', () => {
+  it('Should render modal with backdrop', () => {
     const { queryByTestId } = render(
       <ModalWrapper
         data-testid="ModalWrapper-element"
         hideBackdrop={false}
         isVisible={true}
-        element={element}
-      />
+        children={element}
+      >
+        <div data-testid="modal-element" style={{ backgroundColor: 'none' }}>
+          Hello
+        </div>
+      </ModalWrapper>
     )
+    expect(queryByTestId('modal-element')).not.toBeNull()
     expect(queryByTestId('modal-element')).toHaveAttribute('style', { backgroundColor: 'none' })
   })
 
@@ -23,8 +28,10 @@ describe('ModalWrapper', () => {
         data-testid="ModalWrapper-element"
         hideBackdrop={true}
         isVisible={true}
-        element={element}
-      />
+        children={element}
+      >
+        <div data-testid="modal-element" style={{ backgroundColor: 'rgba(0, 0, 0, 0.7)' }}></div>
+      </ModalWrapper>
     )
     expect(queryByTestId('modal-element')).toHaveAttribute('style', {
       backgroundColor: 'rgba(0, 0, 0, 0.7)'
