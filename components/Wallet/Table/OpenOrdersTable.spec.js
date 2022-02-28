f556f9d21fd3cb14db50f49ce11728ec4f28bcad
Michael J Feher <github@phearzero.com>
2022-02-28 14:32:16 -0600

    🎨 Use waitFor

--- components/Wallet/Table/OpenOrdersTable.spec.js
+++ components/Wallet/Table/OpenOrdersTable.spec.js
@@ -1,42 +1,33 @@
 import React from 'react'
 import { OpenOrdersTable } from './OpenOrdersTable'
-import { render } from '@/test/test-utils'
+import { render, waitFor } from '@/test/test-utils'
 
 const OPEN_ORDER_ROW = 'cancel-order-button'
 const wallet = {
   address: 'TJFFNUYWHPPIYDE4DGGYPGHWKGAPJEWP3DGE5THZS3B2M2XIAPQ2WY3X4I'
 }
 
-describe('OpenOrders', () => {
-  it('should not show any rows if no data is provided', () => {
-    const { queryByTestId } = render(<OpenOrdersTable wallet={wallet} />)
+it('should not show any rows if no data is provided', async () => {
+  const { queryByTestId } = render(<OpenOrdersTable wallet={wallet} />)
 
-    expect(queryByTestId(OPEN_ORDER_ROW)).toBeNull()
-  })
+  expect(await waitFor(() => queryByTestId(OPEN_ORDER_ROW))).toBeNull()
+})
 
-  // it('should display empty state if no data is provided', () => {
-  //   const { queryByTestId } = render(<OpenOrdersTable wallet={wallet} />)
-  //
-  //   expect(queryByTestId(EMPTY_STATE)).not.toBeNull()
-  // })
+it('should show rows if data is provided', async () => {
+  const openOrders = [
+    {
+      id: 21547225,
+      date: '2022-01-19 15:11:46',
+      unix_time: 1642626706,
+      price: '1000.0000',
+      pair: 'BTC/ALGO',
+      type: 'BUY',
+      status: 'OPEN',
+      amount: '1000.000'
+    }
+  ]
 
-  it('should show rows if data is provided', () => {
-    const openOrders = [
-      {
-        id: 21547225,
-        date: '2022-01-19 15:11:46',
-        unix_time: 1642626706,
-        price: '1000.0000',
-        pair: 'BTC/ALGO',
-        type: 'BUY',
-        status: 'OPEN',
-        amount: '1000.000'
-      }
-    ]
+  const { queryByTestId } = render(<OpenOrdersTable orders={openOrders} wallet={wallet} />)
 
-    const { queryByTestId } = render(<OpenOrdersTable orders={openOrders} wallet={wallet} />)
-
-    expect(queryByTestId(OPEN_ORDER_ROW)).not.toBeNull()
-    // expect(queryByTestId(EMPTY_STATE)).toBeNull()
-  })
+  expect(await waitFor(() => queryByTestId(OPEN_ORDER_ROW))).not.toBeNull()
 })
