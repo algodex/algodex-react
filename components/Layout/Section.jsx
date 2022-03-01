fba23e28850b10fbf1590e559aaa85bbb611f464
Michael J Feher <github@phearzero.com>
2022-03-01 15:50:44 -0600

    💄 Remove borders

--- components/Layout/Section.jsx
+++ components/Layout/Section.jsx
@@ -1,33 +1,13 @@
 import styled from '@emotion/styled'
-import { css } from '@emotion/react'
+
 export const Section = styled.section`
   height: 100%;
   width: 100%;
   grid-area: ${({ area }) => area};
-  border-color: ${({ borderColor }) => borderColor} !important;
-  border: ${({ border }) => border};
-  display: ${({ active }) => (active ? 'flex' : 'none')};
-  ${({ mdAndUp }) =>
-    mdAndUp &&
-    css`
-      @media (max-width: 1024px) {
-        display: none;
-      }
-    `}
-  ${({ lgOnly }) =>
-    lgOnly &&
-    css`
-      @media (max-width: 1536px) {
-        display: none;
-      }
-    `}
+  display: ${({ flex }) => (flex ? 'flex' : 'block')};
 `
 
 Section.defaultProps = {
-  active: true,
   area: 'content',
-  'border-color': 'inherit',
-  border: 'inherit',
-  mdAndUp: false,
-  lgOnly: false
+  flex: true
 }
