0153a8990824998e47b304144e41c86cafbe9b69
Michael J Feher <github@phearzero.com>
2022-03-01 15:58:28 -0600

    🎨 Clean up logs in test, remove merge conflict

--- services/algodex.js
+++ services/algodex.js
@@ -26,8 +26,8 @@ export const PUBLIC_API =
 
 export const API_HOST = `${PUBLIC_API}/algodex-backend`
 
-DEBUG && console.debug('process.env.NEXT_PUBLIC_API: ' + process.env.NEXT_PUBLIC_API)
-console.debug('API_HOST: ' + API_HOST)
+// DEBUG && console.debug('process.env.NEXT_PUBLIC_API: ' + process.env.NEXT_PUBLIC_API)
+// console.debug('API_HOST: ' + API_HOST)
 
 let urlToEtag
 let urlToLastResp
