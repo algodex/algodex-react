5e8eb947cbfb08e07881431eaa71a8747850a04f
Ikechi <kechieleazar@gmail.com>
2022-03-04 13:41:51 +0100

    🐛 Fixing search dropdown on mobile

--- components/Layout/OriginalLayout.jsx
+++ components/Layout/OriginalLayout.jsx
@@ -1,19 +1,20 @@
-import PropTypes from 'prop-types'
-import Button from '@/components/Button'
-import styled from '@emotion/styled'
-import detectMobileDisplay from '@/utils/detectMobileDisplay'
 import { useEffect, useRef, useState } from 'react'
 
 import AssetSearch from '@/components/Nav/SearchSidebar'
+import Button from '@/components/Button'
 import OrderBook from '@/components/Asset/OrderBook'
 import Orders from '@/components/Wallet/WalletTabs'
 import PlaceOrder from '@/components/Wallet/PlaceOrder/Original'
+import PropTypes from 'prop-types'
 import Spinner from '@/components/Spinner'
 import TradeHistory from '@/components/Asset/TradeHistory'
 import Wallet from '@/components/Wallet/Connect/WalletConnect'
+import detectMobileDisplay from '@/utils/detectMobileDisplay'
+import styled from '@emotion/styled'
+import useDebounce from '@/hooks/useDebounce'
 import { useEvent } from 'hooks/useEvents'
 import useTranslation from 'next-translate/useTranslation'
-import useDebounce from '@/hooks/useDebounce'
+
 const WalletSection = styled.section`
   grid-area: 1 / 1 / 3 / 3;
   border-left: 1px solid ${({ theme }) => theme.colors.gray['700']};
@@ -47,7 +48,7 @@ const ContentSection = styled.section`
 `
 
 const AssetsSection = styled.section`
-  overflow: hidden;
+  // overflow: hidden;
   @media (min-width: 1536px) {
     display: flex !important;
   }
@@ -288,7 +289,7 @@ function MainLayout({ asset, children }) {
           <PlaceOrder asset={asset} />
         </PlaceOrderSection>
         <SearchAndChartSection active={activeMobile === TABS.CHART}>
-          <AssetsSection>
+          <AssetsSection style={{height: '100rem'}}>
             <AssetSearch style={{ height: '6rem' }} className="h-24" gridRef={gridRef} />
           </AssetsSection>
           <ContentSection>{children}</ContentSection>
