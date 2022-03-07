1d77bcca98cc9c1dc993f5175e37bffa1eec22a1
Michael J Feher <github@phearzero.com>
2022-03-07 14:03:13 -0600

    🐛 Don't redirect _next assets

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -9,12 +9,21 @@ import { NextResponse } from 'next/server'
  */
 export function middleware(request) {
   const url = request.nextUrl.clone()
-  const country = request.geo.country || 'GB'
+  const country = request.geo.country || 'US'
   const to = `/restricted/${country}`
+
+  const matches = url.pathname.match(/svg|json|png|css|_next/g)
+  const isStatic = typeof matches !== 'undefined' ? matches.length > 0 : false
+
   if (
+    // Next loaders
+    !isStatic &&
+    // Not already on restricted
+    url.pathname !== to &&
+    // Mainnet check
     // process.env.NEXT_PUBLIC_ALGORAND_NETWORK === 'mainnet' &&
-    BLOCKED_COUNTRIES.includes(country) &&
-    url.pathname !== to
+    // Blocked country check
+    BLOCKED_COUNTRIES.includes(country)
   ) {
     url.pathname = to
     return NextResponse.redirect(url)
