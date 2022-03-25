7798b19ce28884dc43f796a685b42a92f7c97390
Treasurechic <babychic140@gmail.com>
2022-03-25 18:44:27 +0100

    :white_check_mark: Fixed failed test

--- components/Wallet/PriceConversion/USDPrice.spec.js
+++ components/Wallet/PriceConversion/USDPrice.spec.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import { render } from 'test/test-utils'
-import { UsdPrice } from './UsdPrice'
+import USDPrice from './USDPrice'
 
 const algoPrice = 2.3423
 
-describe('usdPrice', () => {
+describe('USDPrice', () => {
   it('should show price once it gets data', () => {
     const { queryByTestId } = render(
-      <UsdPrice data-testid="usdprice-element" algoPrice={algoPrice} />
+      <USDPrice data-testid="USDprice-element" algoPrice={algoPrice} />
     )
 
-    expect(queryByTestId('sdprice-element')).not.toBeNull()
+    expect(queryByTestId('USDprice-element')).not.toBeNull()
   })
 })
