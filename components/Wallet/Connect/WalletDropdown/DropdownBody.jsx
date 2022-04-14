4460941f5af8f2850d040bb5e0e831ba0043e32e
Ikechi <kechieleazar@gmail.com>
2022-04-14 15:24:56 +0100

    🎨 Switching view to change wallet type

--- components/Wallet/Connect/WalletDropdown/DropdownBody.jsx
+++ components/Wallet/Connect/WalletDropdown/DropdownBody.jsx
@@ -1,15 +1,13 @@
 import ActiveWalletList from './ActiveWalletList'
+import Button from '@mui/material/Button'
 import InactiveWalletsList from './InactiveWalletsList'
 import PropTypes from 'prop-types'
 import WalletOptionsList from './WalletOptionsList'
 import theme from 'theme'
+import { useState } from 'react'
 
-const DropdownBody = ({
-  connectWallet,
-  disconnectWalletFn,
-  activeWalletAddress,
-  sortedWalletsList
-}) => {
+const DropdownBody = ({ activeWalletAddress, sortedWalletsList }) => {
+  const [isConnectingAddress, setIsConnectingAddress] = useState(false)
   return (
     <div
       className="p-2"
@@ -17,18 +15,26 @@ const DropdownBody = ({
         backgroundColor: theme.colors.gray['600']
       }}
     >
-      {!activeWalletAddress && <WalletOptionsList />}
-      {activeWalletAddress && (
+      {(!activeWalletAddress || isConnectingAddress) && (
+        <WalletOptionsList
+          isConnectingAddress={isConnectingAddress}
+          setIsConnectingAddress={setIsConnectingAddress}
+        />
+      )}
+      {activeWalletAddress && !isConnectingAddress && (
         <>
-          <ActiveWalletList
-            wallet={sortedWalletsList?.activeWallet}
-            disconnectWalletFn={disconnectWalletFn}
-          />
-          <InactiveWalletsList
-            walletsList={sortedWalletsList?.inactiveWallet}
-            disconnectWalletFn={disconnectWalletFn}
-          />
-          <WalletOptionsList />
+          <ActiveWalletList wallet={sortedWalletsList?.activeWallet} />
+          <InactiveWalletsList walletsList={sortedWalletsList?.inactiveWallet} />
+          <Button
+            className="w-full flex text-xs font-bold justify-center items-center h-8 mt-2 text-white rounded"
+            variant="contained"
+            style={{
+              backgroundColor: theme.colors.gray['700']
+            }}
+            onClick={() => setIsConnectingAddress(!isConnectingAddress)}
+          >
+            CONNECT ANOTHER WALLET
+          </Button>
         </>
       )}
     </div>
@@ -36,25 +42,11 @@ const DropdownBody = ({
 }
 
 DropdownBody.propTypes = {
-  // connectMyAlgoWallet: PropTypes.func,
-  // connectAlgorandMobileWallet: PropTypes.func,
-  // closeFn: PropTypes.func,
-  // activeWalletAddress: PropTypes.string,
-  // allAddresses: PropTypes.any,
-  // disconnectAlgorandWallet: PropTypes.func,
-  // setActiveWalletAddress: PropTypes.func,
-  // activeNetwork: PropTypes.string,
-  // handleDisconnectFn: PropTypes.func
-
-  connectWallet: PropTypes.func,
-  disconnectWalletFn: PropTypes.func,
   activeWalletAddress: PropTypes.string,
   sortedWalletsList: PropTypes.object
 }
 
 DropdownBody.defaultProps = {
-  connectWallet: () => console.log('Hello Connect'),
-  disconnectWalletFn: () => console.log('Hello Connect'),
   activeWalletAddress: ''
 }
 
--- components/Wallet/Connect/WalletDropdown/WalletOptionsList.jsx
+++ components/Wallet/Connect/WalletDropdown/WalletOptionsList.jsx
@@ -1,17 +1,11 @@
-import Button from '@mui/material/Button'
 import Image from 'next/image'
 import PropTypes from 'prop-types'
 import theme from 'theme'
-import { useAlgodex } from '@algodex/algodex-hooks'
 import { useMyAlgoConnect } from '@/hooks/useMyAlgoConnect'
 // import { useWalletConnect } from '@/hooks/useWalletConnect'
-import { useState } from 'react'
 
-const WalletsOptions = () => {
-  const [isConnectingAddress, setIsConnectingAddress] = useState(false)
+const WalletsOptions = ({ isConnectingAddress, setIsConnectingAddress }) => {
   const connect = useMyAlgoConnect()
-  const { wallet, addresses } = useAlgodex()
-  console.log(addresses, 'addresses here')
   // const { connect: peraConnect, disconnect: peraDisconnect } = useWalletConnect()
   // const peraWallet = useWalletConnect()
 
@@ -21,71 +15,63 @@ const WalletsOptions = () => {
   }
   return (
     <>
-      {!wallet?.address || isConnectingAddress ? (
-        <div
-          className="text-xs text-white rounded p-2"
-          style={{
-            backgroundColor: theme.colors.gray['500']
-          }}
-        >
-          <div className="flex justify-between">
-            <p className="font-semibold mb-2">CONNECT A WALLET</p>
-            {isConnectingAddress && (
-              <button onClick={() => setIsConnectingAddress(!isConnectingAddress)}>Go back</button>
-            )}
+      <div
+        className="text-xs text-white rounded p-2"
+        style={{
+          backgroundColor: theme.colors.gray['500']
+        }}
+      >
+        <div className="flex justify-between">
+          <p className="font-semibold">CONNECT A WALLET</p>
+          {isConnectingAddress && (
+            <button
+              className="cursor-pointer font-medium text-white"
+              onClick={() => setIsConnectingAddress(!isConnectingAddress)}
+            >
+              Go back
+            </button>
+          )}
+        </div>
+        <div className="mt-4 ml-4">
+          <div
+            role="button"
+            tabIndex="0"
+            className="cursor-pointer flex items-center mb-2"
+            onClick={() => WALLETS_CONNECT_MAP['pera-wallet']()}
+            onKeyPress={() => console.log('key pressed')}
+          >
+            <Image
+              src="/Official-Algo-Wallet-icon.svg"
+              alt="Algorand Mobile Wallet"
+              width={25}
+              height={25}
+            />
+            <p className="ml-2 font-medium underline">Algorand Mobile Wallet</p>
           </div>
-          <div className="mt-4 ml-4">
-            <div
-              role="button"
-              tabIndex="0"
-              className="cursor-pointer flex items-center mb-2"
-              onClick={() => WALLETS_CONNECT_MAP['pera-wallet']()}
-              onKeyPress={() => console.log('key pressed')}
-            >
-              <Image
-                src="/Official-Algo-Wallet-icon.svg"
-                alt="Algorand Mobile Wallet"
-                width={25}
-                height={25}
-              />
-              <p className="ml-2 font-medium underline">Algorand Mobile Wallet</p>
-            </div>
-            <div
-              className="cursor-pointer flex items-center mb-2"
-              role="button"
-              tabIndex="0"
-              onClick={() => WALLETS_CONNECT_MAP['my-algo-wallet']()}
-              onKeyPress={() => console.log('key pressed')}
-            >
-              <Image src="/My-Algo-Wallet-icon.svg" alt="My Algo Wallet" width={25} height={25} />
-              <p className="ml-2 font-medium underline">My Algo Wallet</p>
-            </div>
+          <div
+            className="cursor-pointer flex items-center mb-2"
+            role="button"
+            tabIndex="0"
+            onClick={() => WALLETS_CONNECT_MAP['my-algo-wallet']()}
+            onKeyPress={() => console.log('key pressed')}
+          >
+            <Image src="/My-Algo-Wallet-icon.svg" alt="My Algo Wallet" width={25} height={25} />
+            <p className="ml-2 font-medium underline">My Algo Wallet</p>
           </div>
         </div>
-      ) : (
-        <Button
-          className="w-full flex text-xs font-bold justify-center items-center h-8 mt-2 text-white rounded"
-          variant="contained"
-          style={{
-            backgroundColor: theme.colors.gray['700']
-          }}
-          onClick={() => setIsConnectingAddress(!isConnectingAddress)}
-        >
-          CONNECT ANOTHER WALLET
-        </Button>
-      )}
+      </div>
     </>
   )
 }
 
 WalletsOptions.propTypes = {
-  handleWalletConnect: PropTypes.func,
-  isRenderingList: PropTypes.bool
+  isConnectingAddress: PropTypes.bool,
+  setIsConnectingAddress: PropTypes.func
 }
 
 WalletsOptions.defaultProps = {
-  handleWalletConnect: () => console.log('Hello here'),
-  isRenderingList: false
+  isConnectingAddress: false,
+  setIsConnectingAddress: () => console.log('Hello here')
 }
 
 export default WalletsOptions
