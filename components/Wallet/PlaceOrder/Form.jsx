45b0a47f90296a164715fcc7d4a12eea4f9fd919
Michael J Feher <github@phearzero.com>
2022-04-18 12:50:20 -0500

    🎨 Available Balance

--- components/Wallet/PlaceOrder/Form.jsx
+++ components/Wallet/PlaceOrder/Form.jsx
@@ -3,58 +3,19 @@ import Tab from '@/components/Tab'
 import { useCallback, useMemo, useState } from 'react'
 import AdvancedOptions from './Form/AdvancedOptions'
 import Slider from '@/components/Input/Slider'
-import Icon from '@/components/Icon/Icon'
-import { Info } from 'react-feather'
 import { default as MUIInputAdornment } from '@mui/material/InputAdornment'
 import OutlinedInput from '@/components/Input/OutlinedInput'
 import { default as MaterialBox } from '@mui/material/Box'
 import { default as MaterialButton } from '@mui/material/Button'
 import PropTypes from 'prop-types'
-import Tooltip from '@/components/Tooltip'
 import { lighten } from 'polished'
 import theme from '../../../theme'
-import styled from '@emotion/styled'
 import useTranslation from 'next-translate/useTranslation'
 import Typography from '@mui/material/Typography'
 import { ButtonGroup } from '@mui/material'
 import { useAlgodex } from '@algodex/algodex-hooks'
 import Spinner from '@/components/Spinner'
-import fromBaseUnits from '@algodex/algodex-sdk/lib/utils/units/fromBaseUnits'
-
-const IconTextContainer = styled.div`
-  display: flex;
-  align-items: center;
-  color: ${({ theme }) => theme.colors.gray['300']};
-`
-
-const AvailableBalance = styled.div`
-  margin-bottom: 1.25rem;
-  padding-bottom: 0.5rem;
-  border-bottom: 1px solid ${({ theme }) => theme.colors.gray['700']};
-`
-
-const BalanceRow = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  margin-bottom: 0.25rem;
-`
-
-const IconButton = styled.button`
-  cursor: pointer;
-  pointer-events: all;
-  border: none;
-  background: transparent;
-  margin-left: 0.125rem;
-  padding: 0;
-  height: 15px;
-
-  svg {
-    height: 15px;
-    fill: ${({ theme }) => theme.colors.gray[500]};
-    color: ${({ theme }) => theme.colors.gray[900]};
-  }
-`
+import AvailableBalance from './Form/AvailableBalance'
 
 /**
  * # 📝 Place Order Form
@@ -241,74 +202,74 @@ export function PlaceOrderForm({ showTitle = true, asset, onSubmit, components: 
               {t('sell')}
             </MaterialButton>
           </ButtonGroup>
-
-          <AvailableBalance>
-            <IconTextContainer style={{ marginBottom: '10px' }}>
-              <Typography variant="bodyCopyTiny" color="gray.500">
-                {t('available-balance')}
-              </Typography>
-              <Tooltip
-                renderButton={(setTriggerRef) => (
-                  <IconButton ref={setTriggerRef} type="button">
-                    <Info />
-                  </IconButton>
-                )}
-              >
-                <section className="flex items-center justify-between mb-1">
-                  <Typography variant="labelMdSpaced" color="gray.300">
-                    {t('orders:available')}:
-                  </Typography>
-                  <IconTextContainer>
-                    <Typography variant="labelMdSpaced" color="gray.300">
-                      {fromBaseUnits(wallet.amount)}
-                    </Typography>
-                    <Icon use="algoLogo" size={0.625} />
-                  </IconTextContainer>
-                </section>
-                <BalanceRow>
-                  <Typography variant="labelMdSpaced" color="gray.300">
-                    {t('total')}:
-                  </Typography>
-                  <IconTextContainer>
-                    <Typography variant="labelMdSpaced" color="gray.300">
-                      {fromBaseUnits(wallet.amount)}
-                    </Typography>
-                    <Icon use="algoLogo" size={0.625} />
-                  </IconTextContainer>
-                </BalanceRow>
-                <BalanceRow>
-                  <Typography
-                    variant="labelSmForm"
-                    component="span"
-                    color="gray.300"
-                    textTransform="initial"
-                  >
-                    &nbsp;*
-                    {t('max-spend-explanation', {
-                      // amount: new Big(wallet.amount).minus(new Big(wallet.amount)).round(6).toString()
-                    })}
-                  </Typography>
-                </BalanceRow>
-              </Tooltip>
-            </IconTextContainer>
-            <BalanceRow>
-              <Typography variant="labelMdLight" color="gray.400">
-                ALGO
-              </Typography>
-              <Typography variant="labelMdLight" color="gray.300">
-                {fromBaseUnits(wallet.amount)}
-              </Typography>
-            </BalanceRow>
-            <BalanceRow>
-              <Typography variant="labelMdLight" color="gray.400">
-                <input style={{ display: 'none' }} disabled={true} name="asset" value={asset.id} />
-                {asset.name || asset.id}
-              </Typography>
-              <Typography variant="labelMdLight" color="gray.300">
-                {/*{hasBalance && wallet?.assets[asset.id]?.balance}*/}
-              </Typography>
-            </BalanceRow>
-          </AvailableBalance>
+          <AvailableBalance wallet={wallet} asset={asset} />
+          {/*<AvailableBalance>*/}
+          {/*  <IconTextContainer style={{ marginBottom: '10px' }}>*/}
+          {/*    <Typography variant="bodyCopyTiny" color="gray.500">*/}
+          {/*      {t('available-balance')}*/}
+          {/*    </Typography>*/}
+          {/*    <Tooltip*/}
+          {/*      renderButton={(setTriggerRef) => (*/}
+          {/*        <IconButton ref={setTriggerRef} type="button">*/}
+          {/*          <Info />*/}
+          {/*        </IconButton>*/}
+          {/*      )}*/}
+          {/*    >*/}
+          {/*      <section className="flex items-center justify-between mb-1">*/}
+          {/*        <Typography variant="labelMdSpaced" color="gray.300">*/}
+          {/*          {t('orders:available')}:*/}
+          {/*        </Typography>*/}
+          {/*        <IconTextContainer>*/}
+          {/*          <Typography variant="labelMdSpaced" color="gray.300">*/}
+          {/*            {fromBaseUnits(wallet.amount)}*/}
+          {/*          </Typography>*/}
+          {/*          <Icon use="algoLogo" size={0.625} />*/}
+          {/*        </IconTextContainer>*/}
+          {/*      </section>*/}
+          {/*      <BalanceRow>*/}
+          {/*        <Typography variant="labelMdSpaced" color="gray.300">*/}
+          {/*          {t('total')}:*/}
+          {/*        </Typography>*/}
+          {/*        <IconTextContainer>*/}
+          {/*          <Typography variant="labelMdSpaced" color="gray.300">*/}
+          {/*            {fromBaseUnits(wallet.amount)}*/}
+          {/*          </Typography>*/}
+          {/*          <Icon use="algoLogo" size={0.625} />*/}
+          {/*        </IconTextContainer>*/}
+          {/*      </BalanceRow>*/}
+          {/*      <BalanceRow>*/}
+          {/*        <Typography*/}
+          {/*          variant="labelSmForm"*/}
+          {/*          component="span"*/}
+          {/*          color="gray.300"*/}
+          {/*          textTransform="initial"*/}
+          {/*        >*/}
+          {/*          &nbsp;**/}
+          {/*          {t('max-spend-explanation', {*/}
+          {/*            // amount: new Big(wallet.amount).minus(new Big(wallet.amount)).round(6).toString()*/}
+          {/*          })}*/}
+          {/*        </Typography>*/}
+          {/*      </BalanceRow>*/}
+          {/*    </Tooltip>*/}
+          {/*  </IconTextContainer>*/}
+          {/*  <BalanceRow>*/}
+          {/*    <Typography variant="labelMdLight" color="gray.400">*/}
+          {/*      ALGO*/}
+          {/*    </Typography>*/}
+          {/*    <Typography variant="labelMdLight" color="gray.300">*/}
+          {/*      {fromBaseUnits(wallet.amount)}*/}
+          {/*    </Typography>*/}
+          {/*  </BalanceRow>*/}
+          {/*  <BalanceRow>*/}
+          {/*    <Typography variant="labelMdLight" color="gray.400">*/}
+          {/*      <input style={{ display: 'none' }} disabled={true} name="asset" value={asset.id} />*/}
+          {/*      {asset.name || asset.id}*/}
+          {/*    </Typography>*/}
+          {/*    <Typography variant="labelMdLight" color="gray.300">*/}
+          {/*      /!*{hasBalance && wallet?.assets[asset.id]?.balance}*!/*/}
+          {/*    </Typography>*/}
+          {/*  </BalanceRow>*/}
+          {/*</AvailableBalance>*/}
 
           {/*<TabsUnstyled sx={{ width: '100%' }} >*/}
           <Tabs
