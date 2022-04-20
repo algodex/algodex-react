e1c962c3c6a0dacf903fc8e6504f5e2df23002c2
Michael Feher <git@phearzero.com>
2022-04-20 14:49:11 -0500

    ✅ Enable chart spec

--- components/Asset/Chart/Chart.spec.js
+++ components/Asset/Chart/Chart.spec.js
@@ -50,7 +50,7 @@ const DEFAULT_DATA = {
   mode: 'candle'
 }
 
-describe.skip('Chart', () => {
+describe('Chart', () => {
   describe('if no price data is passed', () => {
     it('should display the trading pair as LAMP/ALGO', (done) => {
       const { getByTestId } = render(<Chart {...DEFAULT_DATA} />)
