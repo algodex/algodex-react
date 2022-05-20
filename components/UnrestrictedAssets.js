723e2f26ebf32d6eace25a34b26b50f0425c7ff4
Ikechi <kechieleazar@gmail.com>
2022-05-20 12:11:36 +0100

    ⚡️Add new list

---
+++ components/UnrestrictedAssets.js
@@ -0,0 +1,8 @@
+export const UnrestrictedAssets = {
+  31566704: 'USDC', //USDC
+  465865291: 'STLB', //STBL
+  386192725: 'goBTC', //goBTC
+  386195940: 'goETH', //goETH
+  312769: 'USDt', //USDt
+  19386452: 'goETH Testet'
+}
