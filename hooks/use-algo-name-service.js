ec4147c810677560b764c3014d087255efee00f6
G Iacono <alchemist.bounty@protonmail.com>
2022-05-22 21:13:16 -0400

    ⚰️ Remove Dummy Helper in AlgoNameService Custom Hook

--- hooks/use-algo-name-service.js
+++ hooks/use-algo-name-service.js
@@ -2,67 +2,14 @@ import { ANS } from '@algonameservice/sdk'
 import algosdk from 'algosdk'
 import { useState, useEffect } from 'react'
 
-const Helper = {
-  envObjs: null,
-  getAlgodex: function () {
-    if (!this.envObjs) {
-      const environment = this.getEnvironment()
-      const algodexEnvironment = this.getAlgodexEnvironment()
-      // const algodClient = algodexImport.initAlgodClient(algodexEnvironment)
-      // algodexImport.initIndexer(algodexEnvironment)
-      // algodexApiImport.initAlgodClient(algodexEnvironment)
-      // algodexApiImport.initIndexer(algodexEnvironment)
-      const feeReceiverAddress =
-        process.env.NEXT_PUBLIC_FEE_RECEIVER_ADDRESS ||
-        'U5XMP23KCHXNWW77WV32JRYM5XHQMTDZ7JMODDIGSJDSHUR6RYHVRH62AY'
-      const indexerServer =
-        this.getEnvironment() === 'mainnet'
-          ? 'https://algoindexer.algoexplorerapi.io'
-          : 'https://algoindexer.testnet.algoexplorerapi.io'
+const purestakeIndexerToken =
+  process.env.NEXT_PUBLIC_PURE_STAKE_INDEXER_TOKEN || 'VELyABA1dGqGbAVktbew4oACvp0c0298gMgYtYIb'
 
-      const purestakeIndexerServer =
-        this.getEnvironment() === 'mainnet'
-          ? 'https://mainnet-algorand.api.purestake.io/idx2'
-          : 'https://testnet-algorand.api.purestake.io/idx2'
+const purestakeIndexerServer =
+  process.env.NEXT_PUBLIC_PURE_STAKE_INDEXER_API || 'https://mainnet-algorand.api.purestake.io/idx2'
 
-      const purestakeClientServer =
-        this.getEnvironment() === 'mainnet'
-          ? 'https://mainnet-algorand.api.purestake.io/ps2'
-          : 'https://testnet-algorand.api.purestake.io/ps2'
-      const indexerPort = ''
-      const indexerToken = ''
-
-      const purestakeIndexerToken = 'VELyABA1dGqGbAVktbew4oACvp0c0298gMgYtYIb'
-
-      this.envObjs = {
-        // algodClient,
-        indexerServer,
-        indexerPort,
-        indexerToken,
-        environment,
-        algodexEnvironment,
-        feeReceiverAddress,
-        purestakeIndexerServer,
-        purestakeIndexerToken,
-        purestakeClientServer
-      }
-    }
-    return this.envObjs
-  },
-  getAlgodexEnvironment: function () {
-    return process.env.NEXT_PUBLIC_ALGODEX_ENVIRONMENT || 'production'
-  },
-
-  getEnvironment: function () {
-    const algodexEnvironment = this.getAlgodexEnvironment()
-    if (algodexEnvironment === 'production') {
-      return 'mainnet'
-    }
-    return 'testnet'
-  }
-}
-
-const { purestakeIndexerServer, purestakeIndexerToken, purestakeClientServer } = Helper.getAlgodex()
+const purestakeClientServer =
+  process.env.NEXT_PUBLIC_PURE_STAKE_CLIENT_API || 'https://mainnet-algorand.api.purestake.io/ps2'
 
 const client = new algosdk.Algodv2(
   {
@@ -80,8 +27,6 @@ const indexer = new algosdk.Indexer(
   ''
 )
 
-console.log('Env: ', Helper.getAlgodexEnvironment())
-
 const options = {
   socials: false,
   metadata: false,
@@ -96,24 +41,22 @@ export const useAlgoNameService = (wallets) => {
   //indexer and client must point to mainnet
 
   useEffect(() => {
-    if (wallets) {
-      const promises = wallets.map(() => {
-        return sdk
+    const fetchName = async () => {
+      const dump = [...wallets]
+      for (let i = 0; i < wallets.length; i++) {
+        const names = await sdk
           .address('RANDGVRRYGVKI3WSDG6OGTZQ7MHDLIN5RYKJBABL46K5RQVHUFV3NY5DUE')
+          // .address(wallets[i].address)
           .getNames(options)
-      })
-      Promise.allSettled(promises).then((values) => {
-        const dump = [...wallets]
-        values.forEach(({ status, value }, index) => {
-          if (status === 'fulfilled') {
-            dump[index] = {
-              ...dump[index],
-              name: value[0]?.name || dump[index].name
-            }
-          }
-        })
-        setAlgoWallets(dump)
-      })
+
+        if (names[0].name) {
+          dump[i].name = names[0]?.name
+        }
+      }
+      setAlgoWallets(dump)
+    }
+    if (wallets) {
+      fetchName()
     }
   }, [wallets])
 
