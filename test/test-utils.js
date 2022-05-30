9950897cbf6b2f52acfe7434176506b9ec52d817
Treasurechic <babychic140@gmail.com>
2022-05-30 13:27:04 +0100

    :bug: language bug fix

--- test/test-utils.js
+++ test/test-utils.js
@@ -6,7 +6,6 @@ import { ThemeProvider } from '@mui/material/styles'
 import assetsEN from '../locales/en/assets.json'
 import chartEN from '../locales/en/chart.json'
 import commonEN from '../locales/en/common.json'
-import aboutEN from '../locales/en/about.json'
 import createEmotionCache from '@/utils/createEmotionCache'
 import networkNoficationEN from '../locales/en/network-notification.json'
 import ordersEN from '../locales/en/orders.json'
@@ -32,8 +31,7 @@ const Providers = ({ children }) => (
             'place-order': placeOrderEN,
             chart: chartEN,
             'network-notification': networkNoficationEN,
-            wallet: walletEN,
-            about: aboutEN
+            wallet: walletEN
           }}
         >
           {children}
