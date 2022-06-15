72b71a436134fa4f47c33a3c45b566438c894555
Treasurechic <babychic140@gmail.com>
2022-06-15 17:41:31 +0100

    :sparkles: Temporary support page

--- components/SupportForm.jsx
+++ components/SupportForm.jsx
@@ -1,23 +1,23 @@
-// import React from 'react'
-// import styled from '@emotion/styled'
+import React from 'react'
+import styled from '@emotion/styled'
 
-// const SupportWrapper = styled.section`
-//   margin-top: 18vh;
-//   text-align: center;
-//   font-size: 1.3rem;
-//   a {
-//     color: ${({ theme }) => theme.palette.green[400]};
-//     font-weight: 500;
-//   }
-// `
-// export const SupportForm = () => {
-//   return (
-//     <SupportWrapper>
-//       Kindly leave a message for our support at{' '}
-//       <a href="mailTo:support@algodex.com">support@algodex.com </a>
-//     </SupportWrapper>
-//   )
-// }
+const SupportWrapper = styled.section`
+  margin-top: 18vh;
+  text-align: center;
+  font-size: 1.3rem;
+  a {
+    color: ${({ theme }) => theme.palette.green[400]};
+    font-weight: 500;
+  }
+`
+export const SupportForm = () => {
+  return (
+    <SupportWrapper>
+      Kindly leave a message for our support at{' '}
+      <a href="mailTo:support@algodex.com">support@algodex.com </a>
+    </SupportWrapper>
+  )
+}
 
 // import React, { useState } from 'react'
 // import styled from '@emotion/styled'
