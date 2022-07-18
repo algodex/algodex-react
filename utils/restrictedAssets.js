a5ff0edf1a9bce5946ad74b63217a97c382395d5
Ikechi <kechieleazar@gmail.com>
2022-07-18 12:10:44 +0100

    ✨ Add goMINT to tradeable asset

--- utils/restrictedAssets.js
+++ utils/restrictedAssets.js
@@ -4,6 +4,7 @@ const unrestrictedAssets = {
   386192725: 'goBTC', //goBTC
   386195940: 'goETH', //goETH
   793124631: 'gALGO', //gALGO
+  441139422: 'goMINT', //goMINT
   312769: 'USDt', //USDt
   19386452: 'goETH Testet',
   15322902: 'LAMP', // LAMP Testnet
