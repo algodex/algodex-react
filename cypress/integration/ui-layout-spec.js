5c2e1ff1f4a03fa9e26f4233e048abe45a0fd6b3
Ikechi <kechieleazar@gmail.com>
2022-08-18 11:32:08 +0100

    🐛 remove typo error

--- cypress/integration/ui-layout-spec.js
+++ cypress/integration/ui-layout-spec.js
@@ -33,7 +33,7 @@ describe('Landing Page In Potrait Mode', () => {
         cy.get('[data-testid=candleStickChart]').should('be.visible')
       } else {
         cy.viewport(size)
-        cy.visit('/en/about')packa
+        cy.visit('/en/about')
         clear()
         cy.visit('/en/trade/15322902')
         cy.get('[data-testid=modal-accept]')
