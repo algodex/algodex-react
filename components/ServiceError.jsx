f00c4b7ad84fbeba8c92e9811dd183fc678baacc
Alexander Trefonas <atrefonas@gmail.com>
2022-10-10 16:56:31 -0400

    update year

--- components/ServiceError.jsx
+++ components/ServiceError.jsx
@@ -1,6 +1,6 @@
 /* 
  * Algodex Frontend (algodex-react) 
- * Copyright (C) 2022 Algodex VASP (BVI) Corp.
+ * Copyright (C) 2021 - 2022 Algodex VASP (BVI) Corp.
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published
  * by the Free Software Foundation, either version 3 of the License, or
