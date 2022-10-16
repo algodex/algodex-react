818555a18cf8c4d0880485033ac743a7959fda0e
Alexander Trefonas <atrefonas@gmail.com>
2022-10-16 09:36:36 -0400

    memoize price info

--- components/Asset/OrderBook/OrderBookPriceInfo.js
+++ components/Asset/OrderBook/OrderBookPriceInfo.js
@@ -24,28 +24,31 @@ import floatToFixed from '@algodex/algodex-sdk/lib/utils/format/floatToFixed'
 import { formatUSDPrice } from '@/components/helpers'
 import { mdiApproximatelyEqual } from '@mdi/js'
 import { withAlgorandPriceQuery } from '@algodex/algodex-hooks'
+import { useMemo } from 'react'
+
 export function OrderBookPriceInfo({ algoPrice, asset }) {
-  const asaValue = floatToFixed(convertFromAsaUnits(asset?.price_info?.price, asset.decimals))
-  return (
-    <>
-      <Typography variant="h5" color="white">
-        {asaValue}
-      </Typography>
-      {asset && asset.price_info && (
-        <Typography className="ml-3" data-testid="price-info">
-          {(asset?.price_info?.price24Change &&
-            `${floatToFixed(asset?.price_info?.price24Change, 2)}%`) ||
-            '0.00%'}
+  return useMemo(() => {
+    const asaValue = floatToFixed(convertFromAsaUnits(asset?.price_info?.price, asset.decimals))
+    return (
+      <>
+        <Typography variant="h5" color="white">
+          {asaValue}
         </Typography>
-      )}
-      <div className="flex items-center ml-4 text-gray-500">
-        <Icon className="m-0 p-0" path={mdiApproximatelyEqual} title="Approximately" size={0.7} />
-        <Typography variant="subtitle_small_bold">
-          ${formatUSDPrice(algoPrice * asaValue)}
-        </Typography>
-      </div>
-    </>
-  )
+        {asset && asset.price_info && (
+          <Typography className="ml-3" data-testid="price-info">
+            {(asset?.price_info?.price24Change &&
+              `${floatToFixed(asset?.price_info?.price24Change, 2)}%`) ||
+              '0.00%'}
+          </Typography>
+        )}
+        <div className="flex items-center ml-4 text-gray-500">
+          <Icon className="m-0 p-0" path={mdiApproximatelyEqual} title="Approximately" size={0.7} />
+          <Typography variant="subtitle_small_bold">
+            ${formatUSDPrice(algoPrice * asaValue)}
+          </Typography>
+        </div>
+      </>
+  )}, [algoPrice, asset])
 }
 
 OrderBookPriceInfo.propTypes = {
