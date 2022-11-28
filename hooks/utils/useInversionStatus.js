6fdb43f0c4805c823c38fbaee2bf4858102e17f4
Ikechi <kechieleazar@gmail.com>
2022-11-28 10:59:43 +0100

    Add asset support hook

---
+++ hooks/utils/useInversionStatus.js
@@ -0,0 +1,34 @@
+/* 
+ * Algodex Frontend (algodex-react) 
+ * Copyright (C) 2021 - 2022 Algodex VASP (BVI) Corp.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import { useState, useCallback, useMemo } from 'react'
+import { StableAssets } from '@/components/StableAssets'
+
+/**
+ *
+ * @param {number} id
+ * @return {boolean}
+ */
+export function useInversionStatus(id) {
+  const inversionStatus = localStorage.getItem('inversionStatus')
+  if (inversionStatus && inversionStatus === 'true') {
+    return StableAssets.includes(parseInt(id))
+  }
+  return false
+  // return StableAssets.includes(parseInt(id))
+}
+
+export default useInversionStatus
