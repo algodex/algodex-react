6116778972d8872ec190cf05fa8fc1fcd6705467
Eric Sharma <sharma.e@husky.neu.edu>
2023-02-21 20:44:13 -0500

    Test to see why deployment fails

--- components/LaunchPad/Token/CreateToken.tsx
+++ components/LaunchPad/Token/CreateToken.tsx
@@ -156,7 +156,7 @@ export const CreateToken = () => {
   }
 
   // const { myAlgoConnector, peraConnector } = useWallets()
-  const { peraConnector } = useWallets(() => {})
+  const { peraConnector } = useWallets(null)
   const myAlgoConnector = useMyAlgoConnector()
 
   useEffect(() => {
