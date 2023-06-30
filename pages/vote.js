a5973edcf008bbbb5b61f80332a878a834de05aa
Guilloduran <guilloduran@gmail.com>
2023-06-30 12:15:38 -0300

    Vote home page initial components

--- pages/vote.js
+++ pages/vote.js
@@ -1,6 +1,17 @@
+import Head from 'next/head'
+import Header from '../components/Nav/Header'
+import Banner from '../components/Vote/Banner'
+import VoteTabs from '../components/Vote/VoteTabs'
+
 const VotePage = () => {
   return (
     <>
+      <Head>
+        <title>Algodex | Algorand Decentralized Exchange</title>
+      </Head>
+      <Header />
+      <Banner />
+      <VoteTabs />
       <div>Vote page here</div>
     </>
   )
