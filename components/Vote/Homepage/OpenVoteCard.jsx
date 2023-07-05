dff3ff2b66553d35ad9b02f4ed1a134b6a87a387
Guillermo Duran <guilloduran@gmail.com>
2023-07-05 09:46:19 -0300

    Moved Home page components into their own folder

---
+++ components/Vote/Homepage/OpenVoteCard.jsx
@@ -0,0 +1,160 @@
+import React from 'react'
+import styled from '@emotion/styled'
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos'
+
+const CardContainer = styled.article`
+  border-radius: 8px;
+  border: 1px solid white;
+  color: white;
+  font-family: Inter;
+  height: 143px;
+  margin-bottom: 14px;
+  padding-bottom: 14px;
+  padding-left: 14px;
+  padding-top: 18px;
+
+  @media (min-width: 1024px) {
+    height: 155px;
+    margin: auto;
+    margin-bottom: 28px;
+    padding-bottom: 23px;
+    padding-left: 49px;
+    padding-top: 23px;
+    position: relative;
+    width: 93.3%;
+  }
+`
+const HeadingContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`
+const LeftHeadingContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`
+const RightArrowContainer = styled.div`
+  align-items: center;
+  display: flex;
+  padding-bottom: 16px;
+  padding-right: 8px;
+
+  svg {
+    font-size: 16px;
+  }
+
+  @media (min-width: 1024px) {
+    bottom: 0;
+    padding: 0px;
+    position: absolute;
+    right: 28px;
+    top: 0;
+
+    svg {
+      font-size: 28px;
+    }
+  }
+`
+const TitleContainer = styled.div`
+  align-items: center;
+  display: flex;
+  height: 20px;
+`
+const CheckboxContainer = styled.div`
+  display: flex;
+  margin-right: 3px;
+
+  img {
+    height: 19px;
+    left: -2px !important;
+    position: relative;
+  }
+
+  @media (min-width: 1024px) {
+    position: absolute;
+    left: 12px;
+
+    img {
+      height: 26px;
+      position: static;
+    }
+  }
+`
+const Title = styled.h3`
+  font-size: 16px;
+  font-weight: 700;
+  line-height: 19.36px;
+
+  @media (min-width: 1024px) {
+    font-size: 20px;
+    line-height: 24.2px;
+  }
+`
+const VotingDate = styled.p`
+  color: #7a8693;
+  font-size: 12px;
+  font-weight: 500;
+  line-height: 14.52px;
+  margin: 8px 0px;
+  margin-bottom: 10px;
+
+  @media (min-width: 1024px) {
+    font-size: 14px;
+    line-height: 16.94px;
+    margin-top: 11px;
+  }
+`
+const Description = styled.p`
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 3;
+  color: #7a8693;
+  display: -webkit-box;
+  font-size: 12px;
+  font-weight: 500;
+  height: 45px;
+  letter-spacing: 0px;
+  line-height: 14.52px;
+  overflow: hidden;
+  width: 92%;
+
+  @media (min-width: 1024px) {
+    font-size: 14px;
+    height: 51px;
+    letter-spacing: 0px;
+    line-height: 16.94px;
+    margin-top: 0px;
+    width: 90%;
+  }
+`
+
+function OpenVoteCard() {
+  return (
+    <>
+      <CardContainer>
+        <HeadingContainer>
+          <LeftHeadingContainer>
+            <TitleContainer>
+              <CheckboxContainer>
+                <img src={'/VoteCardCheckbox.svg'} alt="Voting Card Checkbox" />
+              </CheckboxContainer>
+              <Title>Title / Question for Vote</Title>
+            </TitleContainer>
+            <VotingDate>Voting ends: June 18, 2023, 14:00 GMT</VotingDate>
+          </LeftHeadingContainer>
+          <RightArrowContainer>
+            <ArrowForwardIosIcon />
+          </RightArrowContainer>
+        </HeadingContainer>
+        <Description>
+          Porta arcu amet ut nunc feugiat mauris. Consectetur nunc ullamcorper tincidunt aenean
+          lobortis nulla nunc facilisis. Gravida neque orci gravida urna et pulvinar. Ullamcorper
+          congue sed libero at quis iaculis proin varius. Sagittis rhoncus condimentum scelerisque
+          gravida. Neque gravida quam in ornare elementum elementum aliquam id. Viverra facilisi
+          sagittis et dictum risus. Ornare risus enim feugiat porttitor tempus gravida
+        </Description>
+      </CardContainer>
+    </>
+  )
+}
+
+export default OpenVoteCard
---
+++ components/Vote/Homepage/VoteTabs.jsx
@@ -0,0 +1,125 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Box from '@mui/material/Box'
+import OpenVoteCard from './OpenVoteCard'
+import styled from '@emotion/styled'
+import Tab from '@mui/material/Tab'
+import Tabs from '@mui/material/Tabs'
+import Typography from '@mui/material/Typography'
+
+const StyledTabs = styled(Tabs)`
+  margin-top: 1px;
+
+  .MuiTabs-flexContainer {
+    justify-content: center;
+
+    @media (min-width: 1024px) {
+      justify-content: space-between;
+      max-width: 540px;
+    }
+  }
+  .Mui-selected {
+    color: white !important;
+  }
+  .MuiTabs-indicator {
+    background-color: #38a169;
+    height: 3px;
+  }
+
+  @media (min-width: 1024px) {
+    margin-top: 19px;
+    padding-left: 155px;
+  }
+`
+const StyledTab = styled(Tab)`
+  color: #718096;
+  font-family: Inter;
+  font-size: 12px;
+  font-weight: 700;
+  justify-content: flex-end;
+  line-height: 14.52px;
+  padding-bottom: 10px !important;
+  text-transform: uppercase;
+
+  @media (min-width: 1024px) {
+    font-size: 14px;
+    line-height: 16.94px;
+    padding-bottom: 18px !important;
+  }
+`
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: '21px' }}>
+          <Typography component="div">{children}</Typography>
+        </Box>
+      )}
+    </div>
+  )
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired
+}
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`
+  }
+}
+
+function VoteTabs() {
+  const [value, setValue] = React.useState(0)
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue)
+  }
+
+  return (
+    <Box sx={{ width: '100%' }}>
+      <Box
+        sx={{
+          borderBottom: 1,
+          borderColor: '#4A5568',
+          alignContent: 'center',
+          marginBottom: '3px',
+          '@media screen and (min-width: 1024px)': {
+            marginBottom: '7px'
+          }
+        }}
+      >
+        <StyledTabs value={value} onChange={handleChange}>
+          <StyledTab label="Open Votes" {...a11yProps(0)} />
+          <StyledTab label="Upcoming Votes" {...a11yProps(1)} />
+          <StyledTab label="Past Votes" {...a11yProps(2)} />
+        </StyledTabs>
+      </Box>
+      <TabPanel value={value} index={0}>
+        <OpenVoteCard />
+        <OpenVoteCard />
+        <OpenVoteCard />
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        <OpenVoteCard />
+        <OpenVoteCard />
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        <OpenVoteCard />
+      </TabPanel>
+    </Box>
+  )
+}
+export default VoteTabs
