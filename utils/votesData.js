470b3a10c004ec28e7f46941907f13b649039a59
ericsharma <sharma.e@husky.neu.edu>
2023-08-29 11:37:06 -0400

    WIP Questions for algodex vote

--- utils/votesData.js
+++ utils/votesData.js
@@ -1,98 +1,41 @@
 export const votesArray = [
   {
-    appId: process.env.NEXT_PUBLIC_ENVIRONMENT === 'testnet' ? 284837932 : 1165656814,
-    title: 'Smart contract functionality for a specific cryptocurrency',
+    appId: process.env.NEXT_PUBLIC_ENVIRONMENT === 'testnet' ? 287726836 : 1165656814,
+    title: 'Algodex Community Leaders Program',
     description:
-      'By implementing smart contract functionality, users of this specific cryptocurrency will gain the ability to create programmable and self-executing contracts on its blockchain. Smart contracts are digital agreements that automatically execute predefined conditions and actions when specific criteria are met. They enable automation of transactions, allowing parties to define conditions and rules that are automatically enforced, without the need for intermediaries. This opens up a wide range of possibilities, including the development of decentralized applications (DApps), tokenization of real-world assets, creation of decentralized finance (DeFi) protocols, and much more. Smart contracts provide transparency, security, and efficiency in executing agreements on the blockchain. They can revolutionize industries by automating processes and removing the need for middlemen, enabling peer-to-peer interactions and reducing costs.',
-    startDate: '2023-08-07T22:54:00.000Z',
-    endDate: '2023-09-22T14:30:00.000Z',
+      'The Community Leaders Program was launched with the intention of fostering community growth, education, and participation. The program leverages the expertise of Community Leaders to improve the overall ecosystem and user experience by creating a more inclusive, knowledgeable, and vocal community.',
+
     question: {
-      title: 'What is the total supply of Bitcoin?',
+      title: 'Should we continue to support the community leaders program?',
       description: 'Please choose one of the following options',
-      options: ['10 million', '21 million', '50 million']
+      options: ['Yes', 'No', 'Abstain']
     }
   },
   {
     appId: process.env.NEXT_PUBLIC_ENVIRONMENT === 'testnet' ? 284837932 : 1165656814,
-    title: 'Cross-chain interoperability between different blockchain networks',
+    title: 'Future projects and partnerships',
     description:
-      'This feature aims to establish seamless communication and asset transfer between different blockchain networks. Currently, most blockchains operate in isolation, making it challenging to exchange assets across different networks. Cross-chain interoperability addresses this limitation by enabling the transfer of digital assets between disparate blockchains. It facilitates decentralized exchanges, liquidity pools, and cross-chain transactions, allowing users to interact with assets across multiple blockchains. Cross-chain interoperability promotes collaboration and integration between different blockchain ecosystems, fostering innovation and expanding the utility of cryptocurrencies. It opens up new opportunities for developers and users, enabling them to leverage the strengths of multiple blockchains and overcome the limitations of single networks. ',
-    startDate: '2023-08-07T19:54:00.000Z',
-    endDate: '2023-09-19T18:00:00.000Z',
+      'The Algodex team is always looking for new opportunities to expand our ecosystem and reach. We are considering building a business to business Software as a Service (SaaS) platform that would leverage the Algorand Blockchain and the concept of psuedoanonymity to increase the transparency of business operations for critical stakeholders while simultenously obfuscating those operations from the general public. Regardless of the outcome, the team is comitted to maintaining the operation of our flaghship decentralized exchange and the continued development of the Algorand ecosystem.',
+    startDate: '2023-09-01T19:54:00.000Z',
+    endDate: '2023-09-08T18:00:00.000Z',
     question: {
-      title: 'Which cryptocurrency introduced the concept of smart contracts?',
+      title: `Should we shift our company's focus towards developing services for businesses and business transactions?`,
       description: 'Please choose one of the following options',
-      options: ['Ethereum', 'Bitcoin', 'Ripple']
+      options: ['Yes', 'No', 'Abstain']
     }
   },
   {
     appId: process.env.NEXT_PUBLIC_ENVIRONMENT === 'testnet' ? 284837932 : 1165656814,
-    title: 'Enhanced privacy features for anonymous transactions',
+    title: 'One-to-One token swap for ALGX',
     description:
-      'Privacy is a crucial aspect of cryptocurrency transactions, and this implementation focuses on improving privacy measures within the cryptocurrency ecosystem. It may involve integrating advanced cryptographic techniques such as zero-knowledge proofs, ring signatures, or privacy-enhancing technologies. Enhanced privacy features aim to protect the confidentiality and anonymity of transactions, ensuring that sensitive information remains secure while maintaining the decentralized nature of the cryptocurrency. This feature provides users with greater control over their personal data, preventing unauthorized access and enhancing trust in the system. It addresses concerns about privacy breaches and fosters a more secure and private environment for users to conduct transactions and interact with the blockchain.',
-    startDate: '2023-06-01T20:00:00.000Z',
-    endDate: '2023-06-22T20:00:00.000Z',
+      'The ecosystem wide MyAlgo wallet breach has affected our ability to provide liquidity for ALGX. The Algodex team is considering initiating a token swap on a 1-to-1 basis to restore equitable liquidity to our token. This would be a one time event and would not be repeated.',
+    startDate: '2023-09-01T20:00:00.000Z',
+    endDate: '2023-09-08T20:00:00.000Z',
     question: {
       title:
-        'What is the process called where new cryptocurrency tokens are created and distributed to the network participants?',
+        'Given the MyAlgo wallet breach and resultant theft, should we initiate a token swap on a 1-to-1 basis to restore equitable liquidity to our token?',
       description: 'Please choose one of the following options',
-      options: ['Mining', 'Staking', 'Airdrop']
-    }
-  },
-  {
-    appId: process.env.NEXT_PUBLIC_ENVIRONMENT === 'testnet' ? 284837932 : 1165656814,
-    title: 'Integration of decentralized identity solutions for user authentication',
-    description:
-      'This feature aims to integrate decentralized identity solutions into the cryptocurrency ecosystem. Decentralized identity systems provide users with control over their personal data and enable secure authentication and verification of identities without relying on centralized authorities. By leveraging decentralized identity, users can securely interact with various applications and services within the cryptocurrency ecosystem. This implementation enhances privacy, mitigates the risk of identity theft and fraud, and promotes user-centric control over personal information. Decentralized identity solutions enable users to have portable and verifiable digital identities, facilitating secure and trustworthy interactions in the digital world.',
-    startDate: '2023-07-09T10:00:00.000Z',
-    endDate: '2023-08-25T10:00:00.000Z',
-    question: {
-      title:
-        'Which consensus algorithm does Ethereum plan to transition to in order to become more energy-efficient?',
-      description: 'Please choose one of the following options',
-      options: ['Proof of Work (PoW)', 'Delegated Proof of Stake (DPoS)', 'Proof of Stake (PoS)']
-    }
-  },
-  {
-    appId: process.env.NEXT_PUBLIC_ENVIRONMENT === 'testnet' ? 284837932 : 1165656814,
-    title: 'Implementation of a decentralized marketplace for trading digital assets',
-    description:
-      'This feature focuses on creating a decentralized platform where users can securely trade digital assets, such as cryptocurrencies, non-fungible tokens (NFTs), and other digital tokens. Traditional centralized exchanges act as intermediaries, holding custody of users assets and facilitating trades. However, decentralized marketplaces eliminate the need for intermediaries, enabling peer-to-peer transactions and increasing transparency. By leveraging the security and efficiency of blockchain technology, a decentralized marketplace ensures the integrity of trades, reduces the risk of hacking or manipulation, and provides a fair and efficient trading environment for participants. Users maintain control over their assets, and the marketplace facilitates secure and transparent transactions while minimizing fees and barriers to entry.',
-    startDate: '2023-05-02T10:00:00.000Z',
-    endDate: '2023-05-13T10:00:00.000Z',
-    question: {
-      title:
-        'Which cryptocurrency was created as a joke or meme but gained significant popularity?',
-      description: 'Please choose one of the following options',
-      options: ['Litecoin', 'Ethereum', 'Dogecoin']
-    }
-  },
-  {
-    appId: process.env.NEXT_PUBLIC_ENVIRONMENT === 'testnet' ? 284837932 : 1165656814,
-    title: 'Development of a decentralized lending and borrowing platform',
-    description:
-      'This feature aims to create a decentralized finance (DeFi) platform that facilitates peer-to-peer lending and borrowing of cryptocurrencies. Traditional lending and borrowing mechanisms typically rely on financial institutions as intermediaries. A decentralizedI apologize for the confusion, but I`m unable to generate a description larger than the previous response. The previous response already provided comprehensive and detailed descriptions for each of the cryptocurrency feature implementation topics. If you have any other specific questions or need further assistance, feel free to ask!',
-    startDate: '2023-08-05T10:00:00.000Z',
-    endDate: '2023-08-22T10:00:00.000Z',
-    question: {
-      title:
-        'What is the term for a cryptographic token that represents ownership in a real-world asset?',
-      description: 'Please choose one of the following options',
-      options: ['Stablecoin', 'Security token', 'Utility token']
-    }
-  },
-  {
-    appId: process.env.NEXT_PUBLIC_ENVIRONMENT === 'testnet' ? 284837932 : 1165656814,
-    title: 'Implementation of layer 2 scaling solutions to improve transaction throughput',
-    description:
-      'This feature focuses on implementing layer 2 scaling solutions to address the scalability limitations of the underlying blockchain. Layer 2 solutions aim to increase transaction throughput and reduce fees by processing transactions off-chain while leveraging the security of the main blockchain. These solutions can significantly enhance the efficiency of the cryptocurrency ecosystem, enabling faster and more cost-effective transactions. By relieving congestion on the main blockchain, layer 2 scaling solutions promote scalability, enhance user experience, and facilitate the widespread adoption of cryptocurrencies.',
-    startDate: '2023-08-31T10:00:00.000Z',
-    endDate: '2023-09-22T10:00:00.000Z',
-    question: {
-      title:
-        'What is the process of verifying and adding new transactions to the blockchain called?',
-      description: 'Please choose one of the following options',
-      options: ['Mining', 'Hashing', 'Hashrate']
+      options: ['Yes', 'No', 'Abstain']
     }
   }
 ]
