200ba72ee328a58ddce992a8a66e11027a067f72
ericsharma <sharma.e@husky.neu.edu>
2023-12-22 13:31:07 -0500

    Removed blog section

--- pages/about.js
+++ pages/about.js
@@ -15,16 +15,13 @@
  */
 
 import { AboutFooter } from '@/components/About/Footer'
-import BlogPreview from '@/components/About/blog'
 import { FAQSection } from '@/components/About/FAQ'
 import { Features } from '@/components/About/Features'
 import Head from 'next/head'
 import Header from 'components/Nav/Header'
 import { Hero } from '@/components/About/Hero'
 import { PartnerShip } from '@/components/About/PartnerShip'
-import PropTypes from 'prop-types'
 // import { RoadMap } from '@/components/About/RoadMap'
-import { fetchBlogPosts } from '@/services/cms'
 
 /**
  * About Page
@@ -32,7 +29,7 @@ import { fetchBlogPosts } from '@/services/cms'
  * @returns {JSX.Element}
  * @constructor
  */
-const AboutPage = ({ staticBlogPosts }) => {
+const AboutPage = () => {
   return (
     <>
       <Head>
@@ -41,30 +38,12 @@ const AboutPage = ({ staticBlogPosts }) => {
       <Header />
       <Hero />
       <Features />
-      {/* <RoadMap /> */}
       <PartnerShip />
-      <BlogPreview staticBlogPosts={staticBlogPosts} />
+
       <FAQSection />
       <AboutFooter />
     </>
   )
-}
-export async function getStaticProps() {
-  let staticBlogPosts = []
-  try {
-    staticBlogPosts = await fetchBlogPosts()
-  } catch (error) {
-    console.debug(error)
-    staticBlogPosts = []
-  }
-
-  return {
-    props: { staticBlogPosts }
-  }
-}
-
-AboutPage.propTypes = {
-  staticBlogPosts: PropTypes.array
 }
 
 export default AboutPage
